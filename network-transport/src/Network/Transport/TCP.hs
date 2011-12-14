f62c1526d0d388d50ca315a69a1a5e86d319b952
Nicolas Wu <nick@well-typed.com>
2011-12-14 10:36:03 +0000

    Add TCP transport implementation.

--- network-transport/src/Network/Transport/TCP.hs
+++ network-transport/src/Network/Transport/TCP.hs
@@ -1,28 +1,114 @@
 module Network.Transport.TCP
   ( mkTransport
+  , TCPConfig (..)
   ) where
 
 import Network.Transport
-import Data.Word
 
-data Address = Address String
-type Port    = Word16
+import Control.Concurrent (forkIO)
+import Control.Concurrent.Chan
+import Control.Concurrent.MVar
+import Control.Monad (forever, unless)
+import Data.ByteString.Char8 (ByteString)
+import Data.IntMap (IntMap)
+import Data.Word
+import Network.Socket
+import Safe
+
+import qualified Data.ByteString.Char8 as BS
+import qualified Data.IntMap as IntMap
+import qualified Network.Socket as N
+import qualified Network.Socket.ByteString as NBS
+
+type Chans   = MVar (Int, IntMap (Chan ByteString))
+type ChanId  = Int
 
 -- | This deals with several different configuration properties:
 --   * Buffer size, specified in Hints
 --   * LAN/WAN, since we can inspect the addresses
-data TCPConfig = TCPConfig Hints Port Address Address
+-- Note that HostName could be an IP address, and ServiceName could be
+-- a port number
+data TCPConfig = TCPConfig Hints HostName ServiceName
 
+-- | This creates a TCP connection between a server and a number of
+-- clients. Behind the scenes, the server hostname is passed as the SendAddr
+-- and when a connection is made, messages sent down the SendEnd go
+-- via a socket made for the client that connected.
+-- Messages are all queued using an unbounded Chan.
 mkTransport :: TCPConfig -> IO Transport
-mkTransport tcpConfig =
-  -- create buffer
-  -- allocate listening socket
-  -- fork thread on that socket
+mkTransport (TCPConfig _hints host service) = withSocketsDo $ do
+  channels <- newMVar (0, IntMap.empty)
+  serverAddrs <- getAddrInfo
+    (Just (N.defaultHints { addrFlags = [AI_PASSIVE] }))
+    Nothing
+    (Just service)
+  let serverAddr = case serverAddrs of
+                     [] -> error "mkTransport: getAddrInfo returned []"
+                     as -> head as
+  sock <- socket (addrFamily serverAddr) Stream defaultProtocol
+  bindSocket sock (addrAddress serverAddr)
+  listen sock 5
+  forkIO $ procConnections channels sock
+
   return Transport
-    { newConnectionWith = undefined
-        -- create listening mailbox
-        -- send back mailbox details
+    { newConnectionWith = \_ -> do
+        (chanId, chanMap) <- takeMVar channels
+        chan <- newChan
+        putMVar channels (chanId + 1, IntMap.insert chanId chan chanMap)
+        return (mkSendAddr host service chanId, mkReceiveEnd chan)
     , newMulticastWith = undefined
-    , deserialize = undefined
+    , deserialize = \bs ->
+        case readMay . BS.unpack $ bs of
+          Nothing                      -> error "deserialize: cannot parse"
+          Just (host, service, chanId) -> Just $ mkSendAddr host service chanId
     }
 
+  where
+    mkSendAddr :: HostName -> ServiceName -> ChanId -> SendAddr
+    mkSendAddr host service chanId = SendAddr
+      { connectWith = \_ -> mkSendEnd chanId
+      , serialize   = BS.pack . show $ (host, service, chanId)
+      }
+
+    mkSendEnd :: ChanId -> IO SendEnd
+    mkSendEnd chanId = withSocketsDo $ do
+      serverAddrs <- getAddrInfo Nothing (Just host) (Just service)
+      let serverAddr = case serverAddrs of
+                         [] -> error "mkSendEnd: getAddrInfo returned []"
+                         as -> head as
+      sock <- socket (addrFamily serverAddr) Stream defaultProtocol
+      setSocketOption sock KeepAlive 1
+      N.connect sock (addrAddress serverAddr)
+      NBS.sendAll sock $ BS.pack . show $ chanId
+      return $ SendEnd
+        { Network.Transport.send = \bss -> NBS.sendMany sock bss
+        }
+
+    mkReceiveEnd :: Chan ByteString -> ReceiveEnd
+    mkReceiveEnd chan = ReceiveEnd
+      { -- for now we will implement this as a Chan
+        receive = do
+          bs <- readChan chan
+          return [bs]
+      }
+
+    procConnections :: Chans -> Socket -> IO ()
+    procConnections chans sock = forever $ do
+      (clientSock, _clientAddr) <- accept sock
+      -- decode the first message to find the correct chanId
+      bs <- NBS.recv clientSock 4096
+      case BS.readInt bs of
+        Nothing            -> error "procConnections: cannot parse chanId"
+        Just (chanId, bs') -> do
+          -- lookup the channel
+          (_, chanMap) <- readMVar chans
+          case IntMap.lookup chanId chanMap of
+            Nothing   -> error "procConnections: cannot find chanId"
+            Just chan -> forkIO $ do
+              unless (BS.null bs') $ writeChan chan bs'
+              procMessages chan clientSock
+
+    procMessages :: Chan ByteString -> Socket -> IO ()
+    procMessages chan sock = forever $ do
+      bs <- NBS.recv sock 4096
+      writeChan chan bs
