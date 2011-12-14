fdcb722998e3ae523d67088abf8dee511f19da97
Nicolas Wu <nick@well-typed.com>
2011-12-14 10:45:05 +0000

    Modify example for TCP.

--- examples/DemoProcess.hs
+++ examples/DemoProcess.hs
@@ -3,18 +3,19 @@ module DemoProcess where
 import Control.Distributed.Process
 import Control.Concurrent (threadDelay)
 
-import Network.Transport.MVar (mkTransport)
+import Network.Transport.TCP (mkTransport, TCPConfig (..))
+
+import Debug.Trace
 
 demo1 = do
-  trans <- mkTransport
+  trans <- mkTransport $ TCPConfig undefined "127.0.0.1" "8080"
   node  <- newLocalNode trans
-  print "foo"
+  print "Starting node"
   runProcess node $ do
-    liftIO $ print "bar"
+    liftIO $ print "Starting process"
     lpid1 <- spawnLocal (logger 1)
 --    liftIO $ print "baz"
 --    lpid2 <- spawnLocal (logger 2)
-    
     spawnLocal (chatty "jim1" lpid1)
 --    spawnLocal (chatty "jim2" lpid2)
     spawnLocal (chatty "bob1" lpid1)
@@ -31,5 +32,5 @@ chatty name target = do
 logger :: Int -> Process ()
 logger n = do
   str <- expect
-  liftIO $ putStrLn $ show n ++ ": " ++ str
+  liftIO . putStrLn $ show n ++ ": " ++ str
   logger (n+1)
