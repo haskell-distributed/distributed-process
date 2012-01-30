d89fc727abb31071a662457857fd335460fc3b5c
Nicolas Wu <nick@well-typed.com>
2012-01-30 00:06:24 +0000

    Move mkTransport where clauses to top level.

--- network-transport/src/Network/Transport/TCP.hs
+++ network-transport/src/Network/Transport/TCP.hs
@@ -80,90 +80,89 @@ mkTransport (TCPConfig _hints host service) = withSocketsDo $ do
             sClose sock'))
     }
 
-  where
-    mkSourceAddr :: HostName -> ServiceName -> ChanId -> SourceAddr
-    mkSourceAddr host service chanId = SourceAddr
-      { connectWith = \_ -> mkSourceEnd host service chanId
-      , serialize   = B.encode (host, service, chanId)
-      }
+mkSourceAddr :: HostName -> ServiceName -> ChanId -> SourceAddr
+mkSourceAddr host service chanId = SourceAddr
+  { connectWith = \_ -> mkSourceEnd host service chanId
+  , serialize   = B.encode (host, service, chanId)
+  }
 
-    mkSourceEnd :: HostName -> ServiceName -> ChanId -> IO SourceEnd
-    mkSourceEnd host service chanId = withSocketsDo $ do
-      serverAddrs <- getAddrInfo Nothing (Just host) (Just service)
-      let serverAddr = case serverAddrs of
-                         [] -> error "mkSourceEnd: getAddrInfo returned []"
-                         as -> head as
-      sock <- socket (addrFamily serverAddr) Stream defaultProtocol
-      setSocketOption sock ReuseAddr 1
-      N.connect sock (addrAddress serverAddr)
-      NBS.sendAll sock $ B.encode (fromIntegral chanId :: Int64)
-      return SourceEnd
-        { send = \bss -> do
-            let size = fromIntegral (sum . map BS.length $ bss) :: Int64
-            NBS.sendAll sock (B.encode size)
-            mapM_ (NBS.sendAll sock) bss
-        , closeSourceEnd = sClose sock
-        }
+mkSourceEnd :: HostName -> ServiceName -> ChanId -> IO SourceEnd
+mkSourceEnd host service chanId = withSocketsDo $ do
+  serverAddrs <- getAddrInfo Nothing (Just host) (Just service)
+  let serverAddr = case serverAddrs of
+                     [] -> error "mkSourceEnd: getAddrInfo returned []"
+                     as -> head as
+  sock <- socket (addrFamily serverAddr) Stream defaultProtocol
+  setSocketOption sock ReuseAddr 1
+  N.connect sock (addrAddress serverAddr)
+  NBS.sendAll sock $ B.encode (fromIntegral chanId :: Int64)
+  return SourceEnd
+    { send = \bss -> do
+        let size = fromIntegral (sum . map BS.length $ bss) :: Int64
+        NBS.sendAll sock (B.encode size)
+        mapM_ (NBS.sendAll sock) bss
+    , closeSourceEnd = sClose sock
+    }
 
-    mkTargetEnd :: Chans -> ChanId -> Chan ByteString -> TargetEnd
-    mkTargetEnd chans chanId chan = TargetEnd
-      { -- for now we will implement this as a Chan
-        receive = do
-          bs <- readChan chan
-          return [bs]
-      , closeTargetEnd = do
-          (chanId', chanMap) <- takeMVar chans
-          case IntMap.lookup chanId chanMap of
-            Nothing         -> putMVar chans (chanId', chanMap)
-            Just (_, socks) -> do
-              forM_ socks $ \(threadId, sock) -> do
-                killThread threadId
-                sClose sock
-              let chanMap' = IntMap.delete chanId chanMap
-              putMVar chans (chanId', chanMap')
-      }
-
-    procConnections :: Chans -> Socket -> IO ()
-    procConnections chans sock = forever $ do
-      (clientSock, _clientAddr) <- accept sock
-      -- decode the first message to find the correct chanId
-      bs <- recvExact clientSock 8
-      let chanId = fromIntegral (B.decode bs :: Int64)
+mkTargetEnd :: Chans -> ChanId -> Chan ByteString -> TargetEnd
+mkTargetEnd chans chanId chan = TargetEnd
+  { -- for now we will implement this as a Chan
+    receive = do
+      bs <- readChan chan
+      return [bs]
+  , closeTargetEnd = do
       (chanId', chanMap) <- takeMVar chans
       case IntMap.lookup chanId chanMap of
-        Nothing   -> do
-          putMVar chans (chanId', chanMap)
-          error "procConnections: cannot find chanId"
-        Just (chan, socks) -> do
-          threadId <- forkIO $ procMessages chans chanId chan clientSock
-          let chanMap' = IntMap.insert chanId (chan, (threadId, clientSock):socks) chanMap
+        Nothing         -> putMVar chans (chanId', chanMap)
+        Just (_, socks) -> do
+          forM_ socks $ \(threadId, sock) -> do
+            killThread threadId
+            sClose sock
+          let chanMap' = IntMap.delete chanId chanMap
           putMVar chans (chanId', chanMap')
+  }
 
-    -- This function first extracts a header of type Int64, which determines
-    -- the size of the ByteString that follows. The ByteString is then
-    -- extracted from the socket, and then written to the Chan only when
-    -- complete. A length of -1 indicates that the socket has finished
-    -- communicating.
-    procMessages :: Chans -> ChanId -> Chan ByteString -> Socket -> IO ()
-    procMessages chans chanId chan sock = do
-      sizeBS <- recvExact sock 8
-      let size = B.decode sizeBS
-      if size == -1
-        then do
-          (chanId', chanMap) <- takeMVar chans
-          case IntMap.lookup chanId chanMap of
-            Nothing            -> do
-              putMVar chans (chanId', chanMap)
-              error "procMessages: chanId not found."
-            Just (chan, socks) -> do
-              let socks'   = filter ((/= sock) . snd) socks
-              let chanMap' = IntMap.insert chanId (chan, socks') chanMap
-              putMVar chans (chanId', chanMap')
-          sClose sock
-        else do
-          bs <- recvExact sock size
-          writeChan chan bs
-          procMessages chans chanId chan sock
+procConnections :: Chans -> Socket -> IO ()
+procConnections chans sock = forever $ do
+  (clientSock, _clientAddr) <- accept sock
+  -- decode the first message to find the correct chanId
+  bs <- recvExact clientSock 8
+  let chanId = fromIntegral (B.decode bs :: Int64)
+  (chanId', chanMap) <- takeMVar chans
+  case IntMap.lookup chanId chanMap of
+    Nothing   -> do
+      putMVar chans (chanId', chanMap)
+      error "procConnections: cannot find chanId"
+    Just (chan, socks) -> do
+      threadId <- forkIO $ procMessages chans chanId chan clientSock
+      let chanMap' = IntMap.insert chanId (chan, (threadId, clientSock):socks) chanMap
+      putMVar chans (chanId', chanMap')
+
+-- This function first extracts a header of type Int64, which determines
+-- the size of the ByteString that follows. The ByteString is then
+-- extracted from the socket, and then written to the Chan only when
+-- complete. A length of -1 indicates that the socket has finished
+-- communicating.
+procMessages :: Chans -> ChanId -> Chan ByteString -> Socket -> IO ()
+procMessages chans chanId chan sock = do
+  sizeBS <- recvExact sock 8
+  let size = B.decode sizeBS
+  if size == -1
+    then do
+      (chanId', chanMap) <- takeMVar chans
+      case IntMap.lookup chanId chanMap of
+        Nothing            -> do
+          putMVar chans (chanId', chanMap)
+          error "procMessages: chanId not found."
+        Just (chan, socks) -> do
+          let socks'   = filter ((/= sock) . snd) socks
+          let chanMap' = IntMap.insert chanId (chan, socks') chanMap
+          putMVar chans (chanId', chanMap')
+      sClose sock
+    else do
+      bs <- recvExact sock size
+      writeChan chan bs
+      procMessages chans chanId chan sock
 
 -- | The result of `recvExact sock n` is a `ByteString` of length `n`, received
 -- from `sock`. No more bytes than necessary are read from the socket.
