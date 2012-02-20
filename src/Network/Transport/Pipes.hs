ea0b0c048fb5433ec29e4c2a0145d73b56425cc5
Ryan Newton <rrnewton@gmail.com>
2012-02-19 23:09:26 -0500

    This checkin passes a simple demo test, but it still depends on opening and closing the fifo on every send. This is proving trickier to get right than I thought.

--- src/Network/Transport/Pipes.hs
+++ src/Network/Transport/Pipes.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE BangPatterns, CPP #-}
+{-# LANGUAGE BangPatterns, CPP, ScopedTypeVariables #-}
 
 module Network.Transport.Pipes
   ( mkTransport
@@ -6,7 +6,7 @@ module Network.Transport.Pipes
 
 import Control.Monad (when)
 import Control.Concurrent.MVar
-import Control.Concurrent (threadDelay)
+import Control.Concurrent (threadDelay, forkOS)
 import Data.IntMap (IntMap)
 -- import Data.ByteString.Char8 (ByteString)
 import Data.Word
@@ -19,12 +19,14 @@ import Data.Serialize (encode,decode)
 import Data.List (foldl')
 import Network.Transport
 import System.Random (randomIO)
-import System.IO     (IOMode(ReadMode,AppendMode), openFile, hClose)
+import System.IO     (IOMode(ReadMode,AppendMode,WriteMode,ReadWriteMode), 
+		      openFile, hClose, hPutStrLn, hPutStr, stderr, stdout)
 import System.Posix.Files (createNamedPipe, unionFileModes, ownerReadMode, ownerWriteMode)
 
 -- Option 1: low level unix routines:
 -- Option 2: ByteString-provided IO routines
-#if UNIXIO
+#define UNIXIO
+#ifdef UNIXIO
 -- Added in unix-2.5.1.0.  Let's not depend on this yet:
 -- import qualified System.Posix.IO.ByteString as PIO
 import qualified System.Posix.IO as PIO
@@ -47,16 +49,14 @@ mkTransport = do
   lock <- newMVar ()
   let filename = "/tmp/pipe_"++show uid
   createNamedPipe filename $ unionFileModes ownerReadMode ownerWriteMode
-#if UNIXIO
-  pipe <- PIO.openFd filename PIO.ReadOnly Nothing PIO.defaultFileFlags
-#else 
-  pipe <- openFile filename ReadMode
-#endif
+
+  dbgprint1$ "  Created pipe at location: "++ filename
 
   return Transport
     { newConnectionWith = \ _ -> do
+        dbgprint1$ "  Creating new connection"
         return (mkSourceAddr filename, 
-		mkTargetEnd pipe lock)
+		mkTargetEnd filename lock)
     , newMulticastWith = error "Pipes.hs: newMulticastWith not implemented yet"
     , deserialize = \bs -> return$ mkSourceAddr (BS.unpack bs)
     }
@@ -70,52 +70,85 @@ mkTransport = do
     mkSourceEnd :: String -> IO SourceEnd
     mkSourceEnd filename = do
 --      fd <- openFile filename AppendMode
+--      mv <- onOSThread$ openFile filename WriteMode
       return $ 
       -- Write to the named pipe.  If the message is less than
       -- PIPE_BUF (4KB on linux) then this should be atomic, otherwise
       -- we have to do something more sophisticated.
         SourceEnd
         { send = \bss -> do
-            putStrLn$ "SENDING ... "++ show bss
+            dbgprint1$ "SENDING ... "++ show bss
 
 	    -- This may happen on multiple processes/threads:
-	    let msgsize = foldl' (\n s -> n + BS.length s) 0 bss
+	    let msgsize :: Word32 = fromIntegral$ foldl' (\n s -> n + BS.length s) 0 bss
             when (msgsize > 4096)$ -- TODO, look up PIPE_BUF in foreign code
 	       error "Message larger than blocksize written atomically to a named pipe.  Unimplemented."
             -- Otherwise it's just a simple write:
 	    -- We append the length as a header. TODO - REMOVE EXTRA COPY HERE:
-            fd <- openFile filename AppendMode
-	    BS.hPut fd (BS.concat (encode msgsize : bss))
+
+--            fd <- readMVar mv
+            dbgprint1$ "  (sending... got file descriptor) "
+--            fd <- openFile filename AppendMode
+            fd <- openFile filename WriteMode
+
+-- TODO: Consider nonblocking opening of file to make things simpler.
+
+            let finalmsg = BS.concat (encode msgsize : bss)
+            dbgprint1$ "  Final send msg: " ++ show finalmsg
+	    BS.hPut fd finalmsg
             hClose fd -- TEMP -- opening and closing on each send!
         }
 
 --    mkTargetEnd :: Fd -> MVar () -> TargetEnd
-    mkTargetEnd fd lock = TargetEnd
+    mkTargetEnd filename lock = TargetEnd
       { receive = do
+          dbgprint2$ "Begin receive action..."
           -- This should only happen on a single process.  But it may
           -- happen on multiple threads so we grab a lock.
           takeMVar lock
 #ifdef UNIXIO
-          (bytes,cnt) <- PIO.fdRead fd sizeof_header
+--          fd <- PIO.openFd filename PIO.ReadOnly Nothing PIO.defaultFileFlags
+          fd <- PIO.openFd filename PIO.ReadWrite Nothing PIO.defaultFileFlags
+          let oneread n = do (s,cnt) <- PIO.fdRead fd (fromIntegral n)
+			     return (BS.pack s, fromIntegral cnt)
 #else
-          putStrLn$ "  Attempt read header..."
-          
-	  let rdloop = do 
-               putStr "."
-               hdr <- BS.hGet fd sizeof_header
-	       case BS.length hdr of 
-                 n | n == sizeof_header -> return hdr
-		 0 -> do threadDelay (10*1000)
-			 rdloop 
-                 l -> error$ "Inclomplete read of msg header, only "++ show l ++ " bytes"
-          hdr <- rdloop
-          putStrLn$ "  Got header "++ show hdr ++ " attempt read payload"
-          payload <- case decode hdr of
-		      Left err -> error$ "ERROR: "++ err
-		      Right size -> BS.hGet fd size
-          putStrLn$ "  Got payload "++ show payload
+          fd <- openFile filename ReadMode
+          let oneread n = do bs <- BS.hGet fd n; 
+			     return (bs, BS.length bs)
 #endif
+          dbgprint2$ "  Attempt read header..."
+          
+	  let spinread :: Int -> IO BS.ByteString
+              spinread desired = do 
+--               hPutStr stderr "."
+               (bytes,len) <- oneread desired
+	       case len of 
+                 n | n == desired -> return bytes
+		 0 -> do threadDelay (10*1000)
+			 spinread desired
+                 l -> error$ "Inclomplete read expected either 0 bytes or complete msg ("++
+		             show desired ++" bytes) got "++ show l ++ " bytes"
+
+          hdr <- spinread sizeof_header
+          dbgprint2$ "  Got header "++ show hdr ++ " attempt read payload"
+          payload  <- case decode hdr of
+		        Left err -> error$ "ERROR: "++ err
+			Right size -> spinread (fromIntegral (size::Word32))
+          dbgprint2$ "  Got payload "++ show payload
+
           putMVar lock () 
           return [payload]
       }
 
+-- dbgprint2 = hPutStrLn stdout
+-- dbgprint1 = hPutStrLn stderr
+dbgprint1 _ = return ()
+dbgprint2 _ = return ()
+
+
+-- Execute an action on its own OS thread.  Return an MVar to synchronize on.
+onOSThread :: IO a -> IO (MVar a)
+onOSThread action = do 
+  mv <- newEmptyMVar
+  forkOS (action >>= putMVar mv )
+  return mv
