370105220adc70fe195683be7454c4050207187f
Edsko de Vries <edsko@edsko.net>
2012-05-16 11:02:52 +0100

    Replace network-transport with network-transport-2

--- network-transport/src/Network/Transport.hs
+++ network-transport/src/Network/Transport.hs
@@ -1,109 +1,192 @@
-{-# LANGUAGE CPP #-}
+-- | Network Transport 
+module Network.Transport ( -- * Types
+                           Transport(..)
+                         , EndPoint(..)
+                         , Connection(..)
+                         , Event(..)
+                         , ConnectionId
+                         , Reliability(..)
+                         , MulticastGroup(..)
+                         , EndPointAddress(..)
+                         , MulticastAddress(..)
+                           -- * Error codes
+                         , TransportError(..)
+                         , NewEndPointErrorCode(..)
+                         , ConnectErrorCode(..)
+                         , NewMulticastGroupErrorCode(..)
+                         , ResolveMulticastGroupErrorCode(..)
+                         , SendErrorCode(..)
+                         , EventErrorCode(..)
+                         ) where
 
-module Network.Transport
-  ( Hints (..)
-  , TargetEnd (..)
-  , SourceAddr (..)
-  , SourceEnd (..)
-  , SourceHints (..)
-  , Transport (..)
-  , connect
-  , defaultHints
-  , defaultSourceHints
-  , newConnection
-  , newMulticast
-  ) where
+import Data.ByteString (ByteString)
+import Control.Exception (Exception)
+import Data.Typeable (Typeable)
 
-#ifndef LAZY
-import Data.ByteString.Char8 (ByteString)
-#else
-import Data.ByteString.Lazy.Char8 (ByteString)
-#endif
+--------------------------------------------------------------------------------
+-- Main API                                                                   --
+--------------------------------------------------------------------------------
 
--- | The `Hints` and `SourceHints` provide hints to the underlying transport
--- about the kind of connection that is required. This might include details
--- such as whether the connection is eager or buffered, and the buffer size.
-data Hints     = Hints
-data SourceHints = SourceHints
-
--- | A `Transport` encapsulates the functions required to establish many-to-one
--- and one-to-many connections between clients and servers.
--- The `newConnectionWith` function creates a `TargetEnd` that listens to
--- messages sent using the corresponding `SourceAddr`. This connection is
--- established using a `Hints` value, which provides information about the
--- connection topology.
--- Each `SourceAddr` can be serialised into a `ByteString`, and the `deserialize`
--- function converts this back into a `SourceAddr`.
--- Note that these connections provide reliable and ordered messages.
-data Transport = Transport
-  { newConnectionWith :: Hints -> IO (SourceAddr, TargetEnd)
-  , newMulticastWith  :: Hints -> IO (MulticastSourceEnd, MulticastTargetAddr)
-  , deserialize       :: ByteString -> Maybe SourceAddr
-  , closeTransport    :: IO ()
+-- | To create a network abstraction layer, use one of the
+-- @Network.Transport.*@ packages.
+data Transport = Transport {
+    -- | Create a new end point (heavyweight operation)
+    newEndPoint :: IO (Either (TransportError NewEndPointErrorCode) EndPoint)
+    -- | Shutdown the transport completely 
+  , closeTransport :: IO () 
   }
 
--- | This is a convenience function that creates a new connection on a transport
--- using the default hints.
-newConnection :: Transport -> IO (SourceAddr, TargetEnd)
-newConnection transport = newConnectionWith transport defaultHints
+-- | Network endpoint.
+data EndPoint = EndPoint {
+    -- | Endpoints have a single shared receive queue.
+    receive :: IO Event
+    -- | EndPointAddress of the endpoint.
+  , address :: EndPointAddress 
+    -- | Create a new lightweight connection. 
+  , connect :: EndPointAddress -> Reliability -> IO (Either (TransportError ConnectErrorCode) Connection)
+    -- | Create a new multicast group.
+  , newMulticastGroup :: IO (Either (TransportError NewMulticastGroupErrorCode) MulticastGroup)
+    -- | Resolve an address to a multicast group.
+  , resolveMulticastGroup :: MulticastAddress -> IO (Either (TransportError ResolveMulticastGroupErrorCode) MulticastGroup)
+    -- | Close the endpoint
+  , closeEndPoint :: IO ()
+  } 
 
-newMulticast :: Transport -> IO (MulticastSourceEnd, MulticastTargetAddr)
-newMulticast transport = newMulticastWith transport defaultHints
-
--- | The default `Hints` for establishing a new transport connection.
-defaultHints :: Hints
-defaultHints =  Hints
-
--- | A `SourceAddr` is an address that corresponds to a listening `TargetEnd`
--- initially created using `newConnection`. A `SourceAddr` can be shared between
--- clients by using `serialize`, and passing the resulting `ByteString`.
--- Given a `SourceAddr`, the `connectWith` function creates a `SourceEnd` which
--- can be used to send messages.
-data SourceAddr = SourceAddr
-  { connectWith :: SourceHints -> IO SourceEnd
-  , serialize   :: ByteString
+-- | Lightweight connection to an endpoint.
+data Connection = Connection {
+    -- | Send a message on this connection.
+    send :: [ByteString] -> IO (Either (TransportError SendErrorCode) ())
+    -- | Close the connection.
+  , close :: IO ()
   }
 
--- | This is a convenience function that connects with a given `SourceAddr` using
--- the default hints for sending.
-connect :: SourceAddr -> IO SourceEnd
-connect sourceAddr = connectWith sourceAddr defaultSourceHints
+-- | Event on an endpoint.
+data Event = 
+    -- | Received a message
+    Received ConnectionId [ByteString]
+    -- | Connection closed
+  | ConnectionClosed ConnectionId
+    -- | Connection opened
+  | ConnectionOpened ConnectionId Reliability EndPointAddress 
+    -- | Received multicast
+  | ReceivedMulticast MulticastAddress [ByteString]
+    -- | The endpoint got closed (manually, by a call to closeEndPoint or closeTransport)
+  | EndPointClosed
+    -- | An error occurred 
+  | ErrorEvent (TransportError EventErrorCode)  
+  deriving Show
 
--- | The default `SourceHints` for establishing a `SourceEnd`.
-defaultSourceHints :: SourceHints
-defaultSourceHints = SourceHints
+-- | Connection IDs enable receivers to distinguish one connection from another.
+type ConnectionId = Int
 
--- | A `SourceEnd` provides a `send` function that allows vectored messages
--- to be sent to the corresponding `TargetEnd`.
--- The `close` function closes the connection between this source and the target
--- end. Connections between other sources the target end remain unaffected
-data SourceEnd = SourceEnd
-  { send :: [ByteString] -> IO ()
-  , closeSourceEnd :: IO ()
+-- | Reliability guarantees of a connection.
+data Reliability = 
+    ReliableOrdered 
+  | ReliableUnordered 
+  | Unreliable
+  deriving Show
+
+-- | Multicast group.
+data MulticastGroup = MulticastGroup {
+    -- | EndPointAddress of the multicast group. 
+    multicastAddress     :: MulticastAddress
+    -- | Delete the multicast group completely.
+  , deleteMulticastGroup :: IO ()
+    -- | Maximum message size that we can send to this group.
+  , maxMsgSize           :: Maybe Int 
+    -- | Send a message to the group.
+  , multicastSend        :: [ByteString] -> IO ()
+    -- | Subscribe to the given multicast group (to start receiving messages from the group).
+  , multicastSubscribe   :: IO ()
+    -- | Unsubscribe from the given multicast group (to stop receiving messages from the group).
+  , multicastUnsubscribe :: IO ()
+    -- | Close the group (that is, indicate you no longer wish to send to the group).
+  , multicastClose       :: IO ()
   }
 
--- | A `TargetEnd` provides a `receive` function that allows messages
--- to be received from the corresponding `SourceEnd`s.
--- The `closeTargetEnd` function closes all connections to this target,
--- and all new connections will be refused.
-data TargetEnd = TargetEnd
-  { receive :: IO [ByteString]
-  , closeTargetEnd :: IO ()
-  }
+-- | EndPointAddress of an endpoint.
+newtype EndPointAddress = EndPointAddress { endPointAddressToByteString :: ByteString }
+  deriving (Show, Eq, Ord)
 
-newtype MulticastSourceEnd = MulticastSourceEnd
-  { multicastSource :: ByteString -> IO ()
-  }
+-- | EndPointAddress of a multicast group.
+newtype MulticastAddress = MulticastAddress { multicastAddressToByteString :: ByteString }
+  deriving (Show, Eq, Ord)
 
-newtype MulticastTargetAddr = MulticastTargetAddr
-  { multicastConnect :: IO MulticastTargetEnd
-  }
+--------------------------------------------------------------------------------
+-- Error codes                                                                --
+--------------------------------------------------------------------------------
 
-newtype MulticastTargetEnd = MulticastTargetEnd
-  { multicastReceive :: IO ByteString
-  }
+data TransportError error = TransportError error String
+  deriving (Show, Typeable)
 
--- TODO: Other SourceEnds that might be of use:
--- data UnorderedSourceEnd  -- reliable, unordered
--- data UnreliableSourceEnd -- unreliable, unordered
+-- | Although the functions in the transport API never throw TransportErrors
+-- (but return them explicitly), application code may want to turn these into
+-- exceptions. 
+instance (Typeable err, Show err) => Exception (TransportError err)
 
+-- | Errors during the creation of an endpoint
+data NewEndPointErrorCode =
+    -- | Not enough resources
+    -- (i.e., this could be a temporary local problem)
+    NewEndPointInsufficientResources
+    -- | Failed for some other reason
+    -- (i.e., there is probably no point trying again)
+  | NewEndPointFailed 
+  deriving (Show, Typeable)
+
+-- | Connection failure 
+data ConnectErrorCode = 
+    -- | Could not resolve the address 
+    -- (i.e., this could be a temporary remote problem)
+    ConnectNotFound
+    -- | Insufficient resources (for instance, no more sockets available)
+    -- (i.e., this could be a temporary local problem)
+  | ConnectInsufficientResources 
+    -- | Failed for other reasons (including syntax error)
+    -- (i.e., there is probably no point trying again).
+  | ConnectFailed                
+  deriving (Show, Typeable)
+
+-- | Failure during the creation of a new multicast group
+data NewMulticastGroupErrorCode =
+    -- | Insufficient resources
+    -- (i.e., this could be a temporary problem)
+    NewMulticastGroupInsufficientResources
+    -- | Failed for some other reason
+    -- (i.e., there is probably no point trying again)
+  | NewMulticastGroupFailed
+    -- | Not all transport implementations support multicast
+  | NewMulticastGroupUnsupported
+  deriving (Show, Typeable)
+
+-- | Failure during the resolution of a multicast group
+data ResolveMulticastGroupErrorCode =
+    -- | Multicast group not found
+    -- (i.e., this could be a temporary problem)
+    ResolveMulticastGroupNotFound
+    -- | Failed for some other reason (including syntax error)
+    -- (i.e., there is probably no point trying again)
+  | ResolveMulticastGroupFailed
+    -- | Not all transport implementations support multicast 
+  | ResolveMulticastGroupUnsupported
+  deriving (Show, Typeable)
+
+-- | Failure during sending a message
+data SendErrorCode =
+    -- | Could not send this message
+    -- (but another attempt might succeed)
+    SendUnreachable
+    -- | Send failed for some other reason
+    -- (and retrying probably won't help)
+  | SendFailed            
+  deriving (Show, Typeable)
+
+-- | Error codes used when reporting errors to endpoints (through receive)
+data EventErrorCode = 
+    -- | Failure of the entire endpoint 
+    EventEndPointFailed
+    -- | Transport-wide fatal error
+  | EventTransportFailed
+    -- | Connection to a remote endpoint was lost
+  | EventConnectionLost EndPointAddress [ConnectionId] 
+  deriving Show
