ec1ebfa0b495dbe547d62ea8a38dffab9b862648
Edsko de Vries <edsko@edsko.net>
2012-06-26 19:18:54 +0100

    Whoos, missing files. Sorry.

---
+++ distributed-process/src/Control/Distributed/Process/Internal/TypeRep.hs
@@ -0,0 +1,20 @@
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+module Control.Distributed.Process.Internal.TypeRep () where
+
+import Control.Applicative ((<$>), (<*>))
+import Data.Binary (Binary(get, put))
+import Data.Typeable.Internal (TypeRep(..), TyCon(..))
+import GHC.Fingerprint.Type (Fingerprint(..))
+
+instance Binary Fingerprint where
+  put (Fingerprint hi lo) = put hi >> put lo
+  get = Fingerprint <$> get <*> get 
+
+instance Binary TypeRep where
+  put (TypeRep fp tyCon ts) = put fp >> put tyCon >> put ts
+  get = TypeRep <$> get <*> get <*> get
+
+instance Binary TyCon where
+  put (TyCon hash package modul name) = put hash >> put package >> put modul >> put name
+  get = TyCon <$> get <*> get <*> get <*> get
+
