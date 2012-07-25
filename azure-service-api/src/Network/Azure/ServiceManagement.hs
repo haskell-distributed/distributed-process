b9886766b6e0e327d14490bb3803187addd65257
Edsko de Vries <edsko@edsko.net>
2012-07-25 14:07:21 +0100

    Update to use patched http-conduit

--- azure-service-api/src/Network/Azure/ServiceManagement.hs
+++ azure-service-api/src/Network/Azure/ServiceManagement.hs
@@ -1,8 +1,42 @@
 module Network.Azure.ServiceManagement 
   (
+    -- * Submitting requests
+    SubscriptionId(SubscriptionId)
+  , azureRequest
     -- * Re-export functionality from TLS
-    fileReadCertificate
+  , fileReadCertificate
   , fileReadPrivateKey
   ) where
 
-import Network.TLS.Extra
+import Data.ByteString.Lazy (ByteString)
+import Data.ByteString.Char8 as BSC (pack)
+import Network.TLS (PrivateKey)
+import Network.TLS.Extra (fileReadCertificate, fileReadPrivateKey)
+import Data.Certificate.X509 (X509)
+import Network.HTTP.Conduit 
+  ( parseUrl
+  , clientCertificates
+  , requestHeaders
+  , withManager
+  , Response(Response)
+  , httpLbs
+  )
+import Data.CaseInsensitive as CI (mk)
+
+newtype SubscriptionId = SubscriptionId { unSubscriptionId :: String }
+
+azureRequest :: SubscriptionId -> X509 -> PrivateKey -> String -> IO ByteString
+azureRequest sid cert pkey subURL = do
+  req <- parseUrl ( "https://management.core.windows.net/" 
+                 ++ unSubscriptionId sid
+                 ++ subURL
+                  )
+  withManager $ \manager -> do
+    let req' = req {
+        clientCertificates = [ (cert, Just pkey) ]
+      , requestHeaders     = [ (CI.mk $ BSC.pack "x-ms-version", BSC.pack "2010-10-28")
+                             , (CI.mk $ BSC.pack "content-type", BSC.pack "application/xml")
+                             ]
+      }
+    Response _ _ _ lbs <- httpLbs req' manager 
+    return lbs
