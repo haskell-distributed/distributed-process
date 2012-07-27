818a5b0579ca841c1616ad49b1a8a75778148237
Edsko de Vries <edsko@well-typed.com>
2012-07-27 12:44:08 +0100

    Move test script to the CH backend

---
+++ src/Control/Distributed/Process/Backend/Azure/CommandLine.hs
@@ -0,0 +1,71 @@
+-- Base
+import System.Environment (getArgs, getEnv)
+import System.FilePath ((</>))
+import System.Posix.Types (Fd)
+
+-- SSL
+import Network.Azure.ServiceManagement 
+  ( azureSetup
+  , virtualMachines
+  , vmSshEndpoints
+  )
+
+-- SSH
+import Network.SSH.Client.LibSSH2 
+  ( withSSH2
+  , readAllChannel
+  , retryIfNeeded
+  , Session
+  , Channel
+  )
+import Network.SSH.Client.LibSSH2.Foreign 
+  ( initialize
+  , exit
+  , channelExecute
+  )
+import Codec.Binary.UTF8.String (decodeString)
+
+main :: IO ()
+main = do
+  args <- getArgs
+  case args of
+    ["azure", subscriptionId, pathToCert, pathToKey] ->
+      tryConnectToAzure subscriptionId pathToCert pathToKey
+    ["command", user, host, port, cmd] -> 
+      runCommand user host (read port) cmd
+    _ ->
+      putStrLn "Invalid command line arguments"
+
+--------------------------------------------------------------------------------
+-- Taken from libssh2/ssh-client                                              --
+--------------------------------------------------------------------------------
+
+runCommand :: String -> String -> Int -> String -> IO ()
+runCommand login host port command =
+  ssh login host port $ \fd s ch -> do
+      _ <- retryIfNeeded fd s $ channelExecute ch command
+      result <- readAllChannel fd ch
+      let r = decodeString result
+      print (length result)
+      print (length r)
+      putStrLn r
+
+ssh :: String -> String -> Int -> (Fd -> Session -> Channel -> IO a) -> IO ()
+ssh login host port actions = do
+  _ <- initialize True
+  home <- getEnv "HOME"
+  let known_hosts = home </> ".ssh" </> "known_hosts"
+      public = home </> ".ssh" </> "id_rsa.pub"
+      private = home </> ".ssh" </> "id_rsa"
+  _ <- withSSH2 known_hosts public private login host port $ actions
+  exit
+
+--------------------------------------------------------------------------------
+-- Azure tests                                                                -- 
+--------------------------------------------------------------------------------
+
+tryConnectToAzure :: String -> String -> String -> IO ()
+tryConnectToAzure sid pathToCert pathToKey = do
+  setup <- azureSetup sid pathToCert pathToKey
+  vms <- virtualMachines setup
+  mapM_ print (map vmSshEndpoints vms) 
