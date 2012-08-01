24d773f4c4e495903a4697f71add4c2650bc36f0
Edsko de Vries <edsko@well-typed.com>
2012-08-01 14:51:10 +0100

    Basic implementation of 'run' w/closure
    libssh2 uses withCString which does character encoding under the hood. We need
    to add support for bytestrings to libssh2.

--- src/Control/Distributed/Process/Backend/Azure/CommandLine.hs
+++ src/Control/Distributed/Process/Backend/Azure/CommandLine.hs
@@ -1,10 +1,19 @@
+{-# LANGUAGE TemplateHaskell #-}
+import Prelude hiding (catch)
 import System.Environment (getArgs)
 import System.Exit (exitSuccess, exitFailure)
-import System.IO (hFlush, stdout)
-import Control.Monad (unless, forM, forM_)
+import System.IO 
+  ( hFlush
+  , stdout
+  , stdin
+  , hSetBinaryMode
+  )
+import Data.Binary (decode)  
+import qualified Data.ByteString.Lazy as BSL (ByteString, length, getContents)
+import Control.Monad (unless, forM, forM_, join)
 import Control.Monad.IO.Class (liftIO)
 import Control.Arrow (returnA)
-import Control.Exception (throwIO)
+import Control.Exception (throwIO, SomeException)
 import Control.Distributed.Process.Backend.Azure 
   ( AzureParameters(azureSshUserName)
   , defaultAzureParameters
@@ -37,10 +46,109 @@ import Options.Applicative
   , switch
   )
 import Options.Applicative.Arrows (runA, asA)
-import Control.Distributed.Process (getSelfPid, RemoteTable)
+import Control.Distributed.Process 
+  ( getSelfPid
+  , RemoteTable
+  , Closure
+  , Process
+  , unClosure
+  , catch
+  )
+import Control.Distributed.Process.Closure (remotable, mkClosure)
 import Control.Distributed.Process.Node (newLocalNode, runProcess, initRemoteTable)
 import Network.Transport.TCP (createTransport, defaultTCPParameters)
 
+cprint :: String -> Process ()
+cprint = liftIO . putStrLn
+
+remotable ['cprint]
+
+--------------------------------------------------------------------------------
+-- Main                                                                       -- 
+--------------------------------------------------------------------------------
+
+main :: IO ()
+main = do 
+    _ <- SSH.initialize True
+    cmd <- execParser opts
+    case cmd of
+      List {} -> do
+        params <- azureParameters (azureOptions cmd) Nothing
+        backend <- initializeBackend params 
+        css <- cloudServices backend
+        mapM_ print css
+      CopyTo {} -> do
+        params <- azureParameters (azureOptions cmd) (Just (sshOptions cmd))
+        backend <- initializeBackend params
+        css <- cloudServices backend
+        forM_ (findTarget (target cmd) css) $ \vm -> do
+          putStr (vmName vm ++ ": ") >> hFlush stdout 
+          copyToVM backend vm 
+          putStrLn "Done"
+      CheckMD5 {} -> do
+        params <- azureParameters (azureOptions cmd) (Just (sshOptions cmd))
+        backend <- initializeBackend params
+        css <- cloudServices backend
+        matches <- forM (findTarget (target cmd) css) $ \vm -> do
+          unless (status cmd) $ putStr (vmName vm ++ ": ") >> hFlush stdout
+          match <- checkMD5 backend vm 
+          unless (status cmd) $ putStrLn $ if match then "OK" else "FAILED"
+          return match
+        if and matches
+          then exitSuccess
+          else exitFailure
+      RunOn {} -> do
+        params <- azureParameters (azureOptions cmd) (Just (sshOptions cmd))
+        backend <- initializeBackend params
+        css <- cloudServices backend
+        forM_ (findTarget (target cmd) css) $ \vm -> do
+          putStr (vmName vm ++ ": ") >> hFlush stdout 
+          runOnVM backend vm (port cmd) ($(mkClosure 'cprint) "Hello world") 
+      OnVmCommand (vmCmd@OnVmRun {}) -> do
+        let rtable = __remoteTable initRemoteTable
+        onVmRun rtable (onVmIP vmCmd) (onVmPort vmCmd)
+    SSH.exit
+  where
+    opts = info (helper <*> commandParser)
+      ( fullDesc 
+      & header "Cloud Haskell backend for Azure"
+      )
+
+findTarget :: Target -> [CloudService] -> [VirtualMachine]
+findTarget (CloudService cs) css = 
+  concatMap cloudServiceVMs . filter ((== cs) . cloudServiceName) $ css
+findTarget (VirtualMachine virtualMachine) css =
+  [ vm | vm <- concatMap cloudServiceVMs css
+       , vmName vm == virtualMachine
+  ]
+
+azureParameters :: AzureOptions -> Maybe SshOptions -> IO AzureParameters
+azureParameters opts Nothing = 
+  defaultAzureParameters (subscriptionId opts)
+                         (pathToCert opts)
+                         (pathToKey opts)
+azureParameters opts (Just sshOpts) = do
+  params <- defaultAzureParameters (subscriptionId opts)
+                         (pathToCert opts)
+                         (pathToKey opts)
+  return params { 
+      azureSshUserName = remoteUser sshOpts
+    }
+
+onVmRun :: RemoteTable -> String -> String -> IO ()
+onVmRun rtable host port = do
+  hSetBinaryMode stdin True
+  proc <- BSL.getContents :: IO BSL.ByteString
+  putStrLn $ "Got closure encoding of length " ++ show (BSL.length proc)
+  mTransport <- createTransport host port defaultTCPParameters 
+  case mTransport of
+    Left err -> throwIO err
+    Right transport -> do
+      node <- newLocalNode transport rtable
+      runProcess node $ do
+        liftIO $ putStrLn "Starting remote node"
+        catch (join . unClosure . decode $ proc) (\e -> liftIO $ print (e :: SomeException))
+  
 --------------------------------------------------------------------------------
 -- Command line options                                                       --
 --------------------------------------------------------------------------------
@@ -81,6 +189,7 @@ data Command =
         azureOptions   :: AzureOptions 
       , sshOptions     :: SshOptions 
       , target         :: Target 
+      , port           :: String 
       }
   | OnVmCommand {
         onVmCommand    :: OnVmCommand
@@ -167,6 +276,10 @@ runOnParser = RunOn
   <$> azureOptionsParser
   <*> sshOptionsParser
   <*> targetParser 
+  <*> strOption ( long "port"
+                & metavar "PORT"
+                & help "Port number of the CH instance"
+                )
 
 onVmRunParser :: Parser OnVmCommand
 onVmRunParser = OnVmRun 
@@ -185,88 +298,3 @@ onVmCommandParser = OnVmCommand <$> subparser
       (progDesc "Run the executable"))
   )
 
---------------------------------------------------------------------------------
--- Main                                                                       -- 
---------------------------------------------------------------------------------
-
-main :: IO ()
-main = do 
-    _ <- SSH.initialize True
-    cmd <- execParser opts
-    case cmd of
-      List {} -> do
-        params <- azureParameters (azureOptions cmd) Nothing
-        backend <- initializeBackend params 
-        css <- cloudServices backend
-        mapM_ print css
-      CopyTo {} -> do
-        params <- azureParameters (azureOptions cmd) (Just (sshOptions cmd))
-        backend <- initializeBackend params
-        css <- cloudServices backend
-        forM_ (findTarget (target cmd) css) $ \vm -> do
-          putStr (vmName vm ++ ": ") >> hFlush stdout 
-          copyToVM backend vm 
-          putStrLn "Done"
-      CheckMD5 {} -> do
-        params <- azureParameters (azureOptions cmd) (Just (sshOptions cmd))
-        backend <- initializeBackend params
-        css <- cloudServices backend
-        matches <- forM (findTarget (target cmd) css) $ \vm -> do
-          unless (status cmd) $ putStr (vmName vm ++ ": ") >> hFlush stdout
-          match <- checkMD5 backend vm 
-          unless (status cmd) $ putStrLn $ if match then "OK" else "FAILED"
-          return match
-        if and matches
-          then exitSuccess
-          else exitFailure
-      RunOn {} -> do
-        params <- azureParameters (azureOptions cmd) (Just (sshOptions cmd))
-        backend <- initializeBackend params
-        css <- cloudServices backend
-        forM_ (findTarget (target cmd) css) $ \vm -> do
-          putStr (vmName vm ++ ": ") >> hFlush stdout 
-          runOnVM backend vm 
-          putStrLn "Done"
-      OnVmCommand (vmCmd@OnVmRun {}) -> do
-        let rtable = initRemoteTable
-        onVmRun rtable (onVmIP vmCmd) (onVmPort vmCmd)
-    SSH.exit
-  where
-    opts = info (helper <*> commandParser)
-      ( fullDesc 
-      & header "Cloud Haskell backend for Azure"
-      )
-
-findTarget :: Target -> [CloudService] -> [VirtualMachine]
-findTarget (CloudService cs) css = 
-  concatMap cloudServiceVMs . filter ((== cs) . cloudServiceName) $ css
-findTarget (VirtualMachine virtualMachine) css =
-  [ vm | vm <- concatMap cloudServiceVMs css
-       , vmName vm == virtualMachine
-  ]
-
-azureParameters :: AzureOptions -> Maybe SshOptions -> IO AzureParameters
-azureParameters opts Nothing = 
-  defaultAzureParameters (subscriptionId opts)
-                         (pathToCert opts)
-                         (pathToKey opts)
-azureParameters opts (Just sshOpts) = do
-  params <- defaultAzureParameters (subscriptionId opts)
-                         (pathToCert opts)
-                         (pathToKey opts)
-  return params { 
-      azureSshUserName = remoteUser sshOpts
-    }
-
-onVmRun :: RemoteTable -> String -> String -> IO ()
-onVmRun rtable host port = do
-  mTransport <- createTransport host port defaultTCPParameters 
-  case mTransport of
-    Left err -> throwIO err
-    Right transport -> do
-      node <- newLocalNode transport rtable
-      runProcess node $ do
-        pid <- getSelfPid
-        liftIO . putStrLn $ "Azure controller has pid " ++ show pid
-  
-  
