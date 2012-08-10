ffeece84e3ee52f91b4bb41ac3ef133b59c86ccc
Edsko de Vries <edsko@edsko.net>
2012-08-10 17:31:46 +0100

    Add rank1dynamic+distributed-static to install_all

--- install_all.sh
+++ install_all.sh
@@ -7,5 +7,5 @@ if [ "$CABAL" == "" ]; then
   CABAL=cabal
 fi
 
-$CABAL install --with-ghc=$GHC ./network-transport ./network-transport-tcp ./network-transport-inmemory ./distributed-process ./distributed-process-simplelocalnet --reinstall $*
+$CABAL install --with-ghc=$GHC ./rank1dynamic ./distributed-static ./network-transport ./network-transport-tcp ./network-transport-inmemory ./distributed-process ./distributed-process-simplelocalnet --reinstall $*
 
