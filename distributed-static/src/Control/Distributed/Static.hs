a60a8ca2a8f4d5477d8613e5bafed0944dc7b1be
Edsko de Vries <edsko@edsko.net>
2012-08-16 15:43:52 +0100

    Documentation fix

--- distributed-static/src/Control/Distributed/Static.hs
+++ distributed-static/src/Control/Distributed/Static.hs
@@ -104,7 +104,7 @@
 -- We can now define 'sendIntClosure':
 --
 -- > sendIntClosure :: ProcessId -> Closure (Int -> Process ())
--- > sendIntClosure pid = Closure decoder (encode pid)
+-- > sendIntClosure pid = closure decoder (encode pid)
 -- >   where
 -- >     decoder :: Static (ByteString -> Int -> Process ()) 
 -- >     decoder = sendIntStatic `staticCompose` decodeProcessIdStatic
@@ -169,7 +169,7 @@
 -- so that we can define
 --
 -- > sendClosure :: Static (BinaryDict a) -> Process a -> Closure (a -> Process ())
--- > sendClosure dict pid = Closure decoder (encode pid)
+-- > sendClosure dict pid = closure decoder (encode pid)
 -- >   where
 -- >     decoder :: Static (ByteString -> a -> Process ())
 -- >     decoder = (sendDictStatic `staticApply` dict) `staticCompose` decodeProcessIdStatic 
