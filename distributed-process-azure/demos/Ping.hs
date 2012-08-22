ecb51c1778bed87f080e523a2c6d936eac37a7f9
Edsko de Vries <edsko@well-typed.com>
2012-08-22 09:06:08 +0100

    Add comments from the docs to the demos

--- distributed-process-azure/demos/Ping.hs
+++ distributed-process-azure/demos/Ping.hs
@@ -55,16 +55,26 @@ main :: IO ()
 main = do
   args <- getArgs
   case args of
-    "onvm":args' -> onVmMain __remoteTable args'
+    "onvm":args' -> 
+      -- Pass execution to 'onVmMain' if we are running on the VM
+      onVmMain __remoteTable args'
+
     "list":sid:x509:pkey:_ -> do
+      -- List all available cloud services
+      -- (useful, but not strictly necessary for the example)
       params <- defaultAzureParameters sid x509 pkey 
       css <- cloudServices (azureSetup params)
       mapM_ print css
+
     cmd:sid:x509:pkey:user:cloudService:virtualMachine:port:_ -> do
+      -- Initialize the backend and find the right VM 
       params <- defaultAzureParameters sid x509 pkey 
       let params' = params { azureSshUserName = user }
       backend <- initializeBackend params' cloudService
       Just vm <- findNamedVM backend virtualMachine
+
+      -- The same binary can behave as the client or the server,
+      -- depending on the command line arguments
       case cmd of
         "server" -> spawnOnVM backend vm port ($(mkClosure 'pingServer) ()) 
         "client" -> callOnVM backend vm port $ 
