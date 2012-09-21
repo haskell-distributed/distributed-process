e55bb2dd783cdb33acfbf4d97def035dea4020b7
Edsko de Vries <edsko@well-typed.com>
2012-09-21 10:08:55 +0000

    Generalize throughput test
    Something seems to go wrong though when we sent very large messages. Not yet
    sure what exactly is happening.

--- distributed-process/benchmarks/Throughput.hs
+++ distributed-process/benchmarks/Throughput.hs
@@ -1,29 +1,59 @@
+{-# LANGUAGE DeriveDataTypeable #-}
 import System.Environment
 import Control.Monad
 import Control.Applicative
 import Control.Distributed.Process
 import Control.Distributed.Process.Node
 import Network.Transport.TCP (createTransport, defaultTCPParameters)
-import Data.Binary (encode, decode)
+import Data.Binary
 import qualified Data.ByteString.Lazy as BSL
+import Data.Typeable
+
+data SizedList a = SizedList { size :: Int , elems :: [a] }
+  deriving (Typeable)
+
+instance Binary a => Binary (SizedList a) where
+  put (SizedList sz xs) = put sz >> mapM_ put xs
+  get = do 
+    sz <- get 
+    xs <- getMany sz
+    return (SizedList sz xs)
+
+-- Copied from Data.Binary
+getMany :: Binary a => Int -> Get [a]
+getMany n = go [] n
+ where
+    go xs 0 = return $! reverse xs
+    go xs i = do x <- get
+                 x `seq` go (x:xs) (i-1)
+{-# INLINE getMany #-}
+
+nats :: Int -> SizedList Int
+nats = \n -> SizedList n (aux n)
+  where
+    aux 0 = []
+    aux n = n : aux (n - 1)
 
 counter :: Process ()
 counter = go 0
   where
     go :: Int -> Process () 
     go !n = do
-      b <- expect
-      case b of
-        Nothing   -> go (n + 1)
-        Just them -> send them n >> go 0
+      msg <- receiveWait 
+        [ match $ \xs   -> return . Left . size $ (xs :: SizedList Int)
+        , match $ \them -> return . Right $ them
+        ]
+      case msg of
+        Left n' -> go (n + n')
+        Right them -> send them n >> go 0
 
-count :: Int -> ProcessId -> Process ()
-count n them = do
+count :: (Int, Int) -> ProcessId -> Process ()
+count (packets, size) them = do
   us <- getSelfPid
-  replicateM_ n $ send them (Nothing :: Maybe ProcessId)
-  send them (Just us)
+  replicateM_ packets $ send them (nats size)
+  send them us
   n' <- expect
-  liftIO $ print (n == n')
+  liftIO $ print (packets * size, n' == packets * size)
 
 initialProcess :: String -> Process () 
 initialProcess "SERVER" = do
