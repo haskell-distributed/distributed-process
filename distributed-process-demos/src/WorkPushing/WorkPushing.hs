b1854f5e6d18a3406e1467b64491e493aface1ad
Edsko de Vries <edsko@edsko.net>
2012-09-26 14:06:45 +0100

    Distinguish between master/slave and work-pushing

--- distributed-process-demos/src/WorkPushing/WorkPushing.hs
+++ distributed-process-demos/src/WorkPushing/WorkPushing.hs
@@ -1,4 +1,4 @@
-module MasterSlave where
+module WorkPushing where
 
 import Control.Monad
 import Control.Distributed.Process
@@ -10,8 +10,10 @@ fib = go (0, 1)
     go (!a, !b) !n | n == 0    = a
                    | otherwise = go (b, a + b) (n - 1)
 
-slave :: (ProcessId, Integer) -> Process ()
-slave (pid, n) = send pid (fib n)
+slave :: () -> Process ()
+slave () = forever $ do
+  (pid, n) <- expect
+  send pid (fib n)
 
 remotable ['slave]
 
@@ -19,9 +21,12 @@ master :: Integer -> [NodeId] -> Process Integer
 master n slaves = do
   us <- getSelfPid
 
+  -- Start processes on the slaves that compute Fibonacci numbers
+  slaveProcesses <- forM slaves $ flip spawn ($(mkClosure 'slave) ())
+
   -- Distribute 1 .. n amongst the slave processes 
-  forM_ (zip [1 .. n] (cycle slaves)) $ \(m, them) -> 
-    spawn them ($(mkClosure 'slave) (us, m))
+  forM_ (zip [1 .. n] (cycle slaveProcesses)) $ \(m, them) -> 
+    send them (us, m)
 
   -- Wait for the result
   partials <- replicateM (fromInteger n) (expect :: Process Integer)
