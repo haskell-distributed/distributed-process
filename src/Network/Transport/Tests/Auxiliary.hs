0a0889a719d4b2a050937508e75007159c3748c1
Edsko de Vries <edsko@well-typed.com>
2012-12-03 14:38:21 +0000

    Remove trailing whitespace

--- src/Network/Transport/Tests/Auxiliary.hs
+++ src/Network/Transport/Tests/Auxiliary.hs
@@ -30,7 +30,7 @@ import Network.Transport
 import Network.Transport.Tests.Traced (Traceable(..), traceShow)
 
 -- | Like fork, but throw exceptions in the child thread to the parent
-forkTry :: IO () -> IO ThreadId 
+forkTry :: IO () -> IO ThreadId
 forkTry p = do
   tid <- myThreadId
   forkIO $ catch p (\e -> throwTo tid (e :: SomeException))
@@ -40,17 +40,17 @@ trySome :: IO a -> IO (Either SomeException a)
 trySome = try
 
 -- | Run the given test, catching timeouts and exceptions
-runTest :: String -> IO () -> IO Bool 
+runTest :: String -> IO () -> IO Bool
 runTest description test = do
   putStr $ "Running " ++ show description ++ ": "
   hFlush stdout
   done <- try . timeout 60000000 $ test -- 60 seconds
   case done of
-    Left err        -> failed $ "(exception: " ++ show (err :: SomeException) ++ ")" 
+    Left err        -> failed $ "(exception: " ++ show (err :: SomeException) ++ ")"
     Right Nothing   -> failed $ "(timeout)"
-    Right (Just ()) -> ok 
+    Right (Just ()) -> ok
   where
-    failed :: String -> IO Bool 
+    failed :: String -> IO Bool
     failed err = do
       setSGR [SetColor Foreground Vivid Red]
       putStr "failed "
@@ -58,43 +58,43 @@ runTest description test = do
       putStrLn err
       return False
 
-    ok :: IO Bool 
+    ok :: IO Bool
     ok = do
       setSGR [SetColor Foreground Vivid Green]
       putStrLn "ok"
       setSGR [Reset]
       return True
 
--- | Run a bunch of tests and throw an exception if any fails 
+-- | Run a bunch of tests and throw an exception if any fails
 runTests :: [(String, IO ())] -> IO ()
 runTests tests = do
   success <- foldr (liftM2 (&&) . uncurry runTest) (return True) $ tests
   unless success $ fail "Some tests failed"
 
--- | Random thread delay between 0 and the specified max 
+-- | Random thread delay between 0 and the specified max
 randomThreadDelay :: Int -> IO ()
 randomThreadDelay maxDelay = do
   delay <- randomIO :: IO Int
-  threadDelay (delay `mod` maxDelay) 
+  threadDelay (delay `mod` maxDelay)
 
 --------------------------------------------------------------------------------
--- traceShow instances                                                            -- 
+-- traceShow instances                                                            --
 --------------------------------------------------------------------------------
 
 instance Traceable EndPoint where
   trace = const Nothing
 
 instance Traceable Transport where
-  trace = const Nothing 
+  trace = const Nothing
 
 instance Traceable Connection where
-  trace = const Nothing 
+  trace = const Nothing
 
 instance Traceable Event where
-  trace = traceShow 
+  trace = traceShow
 
 instance Show err => Traceable (TransportError err) where
-  trace = traceShow 
+  trace = traceShow
 
 instance Traceable EndPointAddress where
   trace = traceShow . endPointAddressToByteString
