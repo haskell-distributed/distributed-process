50e0619dd21fdbc398861b8205b9b9fdc6dd56fa
RodLogic <dev@rodlogic.net>
2012-12-09 15:12:11 -0500

    cleaned up junk code

--- src/Control/Distributed/Platform/GenServer.hs
+++ src/Control/Distributed/Platform/GenServer.hs
@@ -81,7 +81,7 @@ import           Data.Typeable                              (Typeable)
 type ServerId = ProcessId
 
 -- | Server monad
-newtype Server s m a = Server {
+newtype Server s a = Server {
     unServer :: ST.StateT s Process a
   }
   deriving (Functor, Monad, ST.MonadState s, MonadIO, Typeable, Applicative)
