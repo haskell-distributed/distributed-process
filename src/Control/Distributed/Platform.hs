e6d4d9fba252ed6fdae9dc0cab61c57b25c2642d
Tim Watson <tim@rabbitmq.com>
2012-12-10 16:46:17 +0000

    Primary API exports begin

---
+++ src/Control/Distributed/Platform.hs
@@ -0,0 +1,11 @@
+-- | [Cloud Haskell Platform]
+--
+module Control.Distributed.Platform
+  (
+    Timeout(..)
+  , TimeInterval
+  , TimeUnit
+  ) where
+
+import Control.Distributed.Platform.Internal.Types
+  
