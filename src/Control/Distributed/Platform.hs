0ad829c1f20a2220465eda94a775766610a1d083
Tim Watson <tim@rabbitmq.com>
2012-12-10 17:12:08 +0000

    Utils/* => Platform/API; doc tidy

--- src/Control/Distributed/Platform.hs
+++ src/Control/Distributed/Platform.hs
@@ -2,7 +2,7 @@
 --
 module Control.Distributed.Platform
   (
-      -- time interval handling
+    -- time interval handling
     milliseconds
   , seconds
   , minutes
@@ -15,4 +15,40 @@ module Control.Distributed.Platform
   ) where
 
 import Control.Distributed.Platform.Internal.Types
-import Control.Distributed.Platform.Utils
+
+--------------------------------------------------------------------------------
+-- API                                                                        --
+--------------------------------------------------------------------------------
+
+-- time interval/unit handling
+
+-- | converts the supplied @TimeInterval@ to milliseconds
+intervalToMs :: TimeInterval -> Int
+intervalToMs (TimeInterval u v) = timeToMs u v
+
+-- | given a number, produces a @TimeInterval@ of milliseconds
+milliseconds :: Int -> TimeInterval
+milliseconds = TimeInterval Millis
+
+-- | given a number, produces a @TimeInterval@ of seconds
+seconds :: Int -> TimeInterval
+seconds = TimeInterval Seconds
+
+-- | given a number, produces a @TimeInterval@ of minutes
+minutes :: Int -> TimeInterval
+minutes = TimeInterval Minutes
+
+-- | given a number, produces a @TimeInterval@ of hours
+hours :: Int -> TimeInterval
+hours = TimeInterval Hours
+
+-- TODO: timeToMs is not exactly efficient and we need to scale it up to
+--       deal with days, months, years, etc
+
+-- | converts the supplied @TimeUnit@ to milliseconds
+timeToMs :: TimeUnit -> Int -> Int
+timeToMs Millis  ms   = ms
+timeToMs Seconds sec  = sec * 1000
+timeToMs Minutes mins = (mins * 60) * 1000
+timeToMs Hours   hrs  = ((hrs * 60) * 60) * 1000
+
