8e8372ad291d1514ef149ec5a0319297f185a987
Tim Watson <tim@rabbitmq.com>
2012-12-12 21:42:04 +0000

    refactor async to use typed channels instead of MVar

--- src/Control/Distributed/Platform/Async.hs
+++ src/Control/Distributed/Platform/Async.hs
@@ -1,22 +1,22 @@
 {-# LANGUAGE DeriveDataTypeable        #-}
 {-# LANGUAGE TemplateHaskell           #-}
-{-# LANGUAGE EmptyDataDecls           #-}
+{-# LANGUAGE EmptyDataDecls            #-}
+{-# LANGUAGE StandaloneDeriving        #-}
 
 module Control.Distributed.Platform.Async
   ( -- types/data
     AsyncRef
   , AsyncWorkerId
   , AsyncGathererId
-  , SpawnAsync
+  , AsyncTask
   , AsyncCancel
   , AsyncData
-  , Async()
+  , Async(worker)
   , AsyncResult(..)
   -- functions for starting/spawning
   , async
   -- and stopping/killing
   , cancel
-  , cancelAsync
   , cancelWait
   -- functions to query an async-result
   , poll
@@ -27,14 +27,15 @@ module Control.Distributed.Platform.Async
   ) where
 
 import Control.Concurrent.MVar
-import Control.Distributed.Platform
+import Control.Distributed.Platform.Timer
   ( sendAfter
   , cancelTimer
+  , intervalToMs
   , TimerRef
-  , TimeInterval()
   )
 import Control.Distributed.Platform.Internal.Types
   ( CancelWait(..)
+  , TimeInterval()
   )
 import Control.Distributed.Process
 import Control.Distributed.Process.Internal.Types
@@ -42,6 +43,9 @@ import Control.Distributed.Process.Internal.Types
   )
 import Control.Distributed.Process.Serializable
 
+import Data.Binary
+import Data.DeriveTH
+import Data.Typeable (Typeable)
 import Data.Maybe
   ( fromMaybe
   )
@@ -59,18 +63,25 @@ type AsyncWorkerId   = AsyncRef
 -- | A reference to an asynchronous "gatherer"
 type AsyncGathererId = AsyncRef
 
--- | A function that takes an 'AsyncGathererId' (to which replies should be
--- sent) and spawns an asynchronous (user defined) action, returning the
--- spawned actions 'AsyncWorkerId' in the @Process@ monad. 
-type SpawnAsync = AsyncGathererId -> Process AsyncWorkerId
+-- | A task to be performed asynchronously. This can either take the
+-- form of an action that runs over some type @a@ in the @Process@ monad,
+-- or a tuple that adds the node on which the asynchronous task should be
+-- spawned - in the @Process a@ case the task is spawned on the local node
+type AsyncTask a = Process a
 
 type AsyncData a = MVar (AsyncResult a)
+
+type InternalChannel a = (SendPort (AsyncResult a), ReceivePort (AsyncResult a))
 
 -- | An asynchronous action spawned by 'async'.
 -- Asynchronous operations are run in a separate process, and
 -- operations are provided for waiting for asynchronous actions to
 -- complete and obtaining their results (see e.g. 'wait').
-data Async a = Async AsyncRef AsyncRef (AsyncData a)
+data Async a = Async {
+    worker    :: AsyncWorkerId
+  , insulator :: AsyncGathererId
+  , channel   :: (InternalChannel a)
+  }
 
 -- | Represents the result of an asynchronous action, which can be in several
 -- states at any given time.
@@ -79,6 +90,19 @@ data AsyncResult a =
   | AsyncFailed DiedReason  -- | a failed action and the failure reason
   | AsyncCancelled          -- | a cancelled action
   | AsyncPending            -- | a pending action (that is still running)
+    deriving (Typeable)
+$(derive makeBinary ''AsyncResult)
+
+deriving instance Eq a => Eq (AsyncResult a)
+
+deriving instance Show a => Show (AsyncResult a)
+
+--instance (Eq a) => Eq (AsyncResult a) where
+--  (AsyncDone   x) == (AsyncDone   x') = x == x'
+--  (AsyncFailed r) == (AsyncFailed r') = r == r'
+--  AsyncCancelled  == AsyncCancelled   = True
+--  AsyncPending    == AsyncPending     = True
+--  _               == _                = False
 
 -- | An async cancellation takes an 'AsyncRef' and does some cancellation
 -- operation in the @Process@ monad.
@@ -99,53 +123,52 @@ type AsyncCancel = AsyncRef -> Process ()
 -- then the 'AsyncResult' will be @AsyncDone ()@ instead of the usual
 -- @AsyncFailed DiedNormal@ which would normally result from this scenario.
 --
-async :: (Serializable a) => SpawnAsync -> Process (Async a)
-async spawnF = do
-    mv  <- liftIO $ newEmptyMVar
-    (wpid, gpid) <- spawnWorkers spawnF mv
-    return (Async wpid gpid mv)
-  where
-    spawnWorkers :: (Serializable a) =>
-                    SpawnAsync -> AsyncData a -> Process (AsyncRef, AsyncRef)
-    spawnWorkers sp ad = do
-      root <- getSelfPid
-      
-      -- listener/response proxy
-      gpid <- spawnLocal $ do
-        proxy  <- getSelfPid
-        worker <- sp proxy
-        
-        send root worker   -- let the parent process know the worker pid
-        
-        monRef <- monitor worker
-        finally (pollUntilExit worker monRef ad) (unmonitor monRef)
-      
-      wpid <- expect
-      return (wpid, gpid)
+async :: (Serializable a) => AsyncTask a -> Process (Async a)
+async t = do
+    (wpid, gpid, chan) <- spawnWorkers t
+    return Async {
+        worker    = wpid
+      , insulator = gpid
+      , channel   = chan
+      }
+
+spawnWorkers :: (Serializable a) =>
+                AsyncTask a ->
+                Process (AsyncRef, AsyncRef, (InternalChannel a))
+spawnWorkers task = do
+    root <- getSelfPid
+    chan <- newChan
+  
+    -- listener/response proxy
+    insulatorPid <- spawnLocal $ do
+        workerPid <- spawnLocal $ do
+            r <- task
+            sendChan (fst chan) (AsyncDone r)
     
+        send root workerPid   -- let the parent process know the worker pid
+    
+        monRef <- monitor workerPid
+        finally (pollUntilExit workerPid monRef chan) (unmonitor monRef)
+  
+    workerPid <- expect
+    return (workerPid, insulatorPid, chan)
+  where  
     -- blocking receive until we see an input message
     pollUntilExit :: (Serializable a) =>
-                     ProcessId -> MonitorRef -> AsyncData a -> Process ()
-    pollUntilExit pid ref ad = do
+                     ProcessId -> MonitorRef -> InternalChannel a -> Process ()
+    pollUntilExit pid ref (replyTo, _) = do
         r <- receiveWait [
             matchIf
                 (\(ProcessMonitorNotification ref' pid' _) ->
                     ref' == ref && pid == pid')
                 (\(ProcessMonitorNotification _    _ r) -> return (Right r))
-          , match (\x -> return (Left x))
+          , match (\c@(CancelWait) -> kill pid "cancel" >> return (Left c))
           ]
         case r of
-            Left  a          -> liftIO $ putMVar ad (AsyncDone a)   
-            Right DiedNormal -> pollUntilExit pid ref ad -- note [recursion]
-            Right d          -> liftIO $ putMVar ad (AsyncFailed d)
+            Left  CancelWait -> sendChan replyTo AsyncCancelled   
+            Right DiedNormal -> return ()
+            Right d          -> sendChan replyTo (AsyncFailed d)
             
--- note [recursion]
--- We recurse /just once/ if we've seen a normal exit from our worker. We're
--- absolutely sure about this, because once we've seen DiedNormal for the
--- monitored process, it's not possible to see another monitor signal for it.
--- Based on this, the only other kinds of message that can arrive are the
--- return value from the worker or a cancellation from the coordinating process.
-
 -- | Check whether an 'Async' has completed yet. The status of the asynchronous
 -- action is encoded in the returned 'AsyncResult'. If the action has not
 -- completed, the result will be 'AsyncPending', or one of the other
@@ -153,15 +176,16 @@ async spawnF = do
 -- Use 'wait' or 'waitTimeout' if you need blocking/waiting semantics.
 -- See 'Async'.
 poll :: (Serializable a) => Async a -> Process (AsyncResult a)
-poll (Async _ _ d) =
-  liftIO $ tryTakeMVar d >>= return . fromMaybe (AsyncPending)
+poll hAsync = do
+  r <- receiveChanTimeout 0 $ snd (channel hAsync)
+  return $ fromMaybe (AsyncPending) r
 
 -- | Like 'poll' but returns 'Nothing' if @(poll hAsync) == AsyncPending@.
 -- See 'poll'.
 check :: (Serializable a) => Async a -> Process (Maybe (AsyncResult a))
 check hAsync = poll hAsync >>= \r -> case r of
-    AsyncPending -> return Nothing
-    ar           -> return (Just ar)  
+  AsyncPending -> return Nothing
+  ar           -> return (Just ar)  
 
 -- | Wait for an asynchronous operation to complete or timeout. This variant
 -- returns the 'AsyncResult' itself, which will be 'AsyncPending' if the
@@ -175,12 +199,7 @@ waitCheckTimeout t hAsync =
 -- value. The outcome of the action is encoded as an 'AsyncResult'.
 --
 wait :: (Serializable a) => Async a -> Process (AsyncResult a)
-wait hAsync =
-    nullTimerRef >>= waitAux hAsync >>= return . fromMaybe (AsyncPending)
-  where nullTimerRef :: Process TimerRef
-        nullTimerRef = do
-          nid <- getSelfNode
-          return (nullProcessId nid)
+wait hAsync = receiveChan $ snd (channel hAsync)
 
 -- | Wait for an asynchronous operation to complete or timeout. Returns
 -- @Nothing@ if the 'AsyncResult' does not change from @AsyncPending@ within
@@ -189,83 +208,18 @@ wait hAsync =
 -- consider using 'wait' or 'waitCheckTimeout' instead. 
 waitTimeout :: (Serializable a) =>
                TimeInterval -> Async a -> Process (Maybe (AsyncResult a))
-waitTimeout t hAsync = do
-  self <- getSelfPid
-  ar <- poll hAsync
-  case ar of
-      AsyncPending -> sendAfter t self CancelWait >>= waitAux hAsync
-      _            -> return (Just ar)
+waitTimeout t hAsync =
+  receiveChanTimeout (intervalToMs t) $ snd (channel hAsync)
 
--- | Private /wait/ implementation. 
-waitAux :: (Serializable a) =>
-           Async a -> TimerRef -> Process (Maybe (AsyncResult a))
-waitAux a ref =
-  getSelfPid >>= spawnWait a ref >> (do "finished" <- expect; check a)  
-    where
-      spawnWait :: (Serializable a) => 
-                   Async a -> TimerRef -> ProcessId -> Process (ProcessId)
-      spawnWait a' ref' pid = spawnLocal $ waitLoop a' ref' pid
-      -------------------
-      waitLoop :: (Serializable a) =>
-                  Async a -> TimerRef -> ProcessId -> Process ()
-      waitLoop asyncHandle tRef wAuxPid = do
-        m <- waitOne asyncHandle        -- note [wait loop]
-        case m of
-            Just _  -> finally (send wAuxPid "finished") (cancelTimer tRef)
-            Nothing -> waitLoop asyncHandle ref wAuxPid 
-      -------------------    
-      waitOne :: (Serializable a) => Async a -> Process (Maybe (AsyncResult a))
-      waitOne a' = do
-        m <- receiveTimeout 0 [
-            match (\CancelWait -> return AsyncPending)]
-        case m of
-            Nothing -> check a'     -- if we timed out, check the result again
-            Just _  -> return m     -- (Just CancelWait) means we're done here
+-- | Cancel an asynchronous operation. To wait for cancellation to complete, use
+-- 'cancelWait' instead.
+cancel :: Async a -> Process ()
+cancel (Async _ g _) = send g CancelWait
 
--- note [wait loop]
--- This logic is a bit spaghetti-like so a little explanation:
--- Firstly, we spawn a /waiter/ process so that timing out is simple.
--- Doing this by creating a loop in the caller's process is just a mess,
--- as should be obvious from the necessary type signature once you try it.
+-- | Cancel an asynchronous operation and wait for the cancellation to complete.
+-- Because of the asynchronous nature of message passing, the instruction to
+-- cancel will race with the asynchronous worker, so it is /entirely possible/
+-- that the 'AsyncResult' returned will not necessarily be 'AsyncCancelled'.
 --
--- Instead, the /waiter/ queries its mailbox continually and defers to 'check'
--- to see if the AsyncResult is ready yet. Once we finally get a result back
--- from 'check', we signal the caller's process with "finished" after which
--- they can pull the results without delay by calling 'check' again.
---
--- This logic can easily be made to block indefinitely by simply not setting up
--- a timer to signal us with @CancelWait@ - remember to pass an /empty/
--- process identifier (i.e., nullProcessId) in that case however, otherwise
--- the call to 'cancelTimer' might crash.
-
--- | Cancel an asynchronous operation. The cancellation method to be used
--- is passed in @asyncCancel@ and can be synchronous (see 'cancelWait') or
--- asynchronous (see 'cancelAsync'). The latter is truly asynchronous, in the
--- same way that message passing is asynchronous, whilst the former will block
--- until a @ProcessMonitorNotification@ is received for all participants in the
--- @Async@ action.
-cancel :: Async a -> AsyncCancel -> Process ()
-cancel (Async w g d) asyncCancel = do
-    asyncCancel w
-    asyncCancel g
-    liftIO $ tryPutMVar d AsyncCancelled >> return ()
-
--- | Given an @AsyncRef@, will kill the associated process. This call returns
--- immediately.
-cancelAsync :: AsyncCancel
-cancelAsync = (flip kill) "cancelled"
-
--- | Given an @AsyncRef@, will kill the associated process and block until
--- a @ProcessMonitorNotification@ is received, confirming that the process has
--- indeed died. Passing an @AsyncRef@ for a process that has already died is
--- not an error and will not block, so long as the monitor implementation
--- continues to support this. 
-cancelWait :: AsyncCancel
-cancelWait pid = do
-  ref <- monitor pid
-  cancelAsync pid
-  receiveWait [
-    matchIf (\(ProcessMonitorNotification ref' pid' _) ->
-                ref' == ref && pid' == pid)
-            (\(ProcessMonitorNotification _ _ r) -> return r) ] >> return ()
-  
+cancelWait :: (Serializable a) => Async a -> Process (AsyncResult a)
+cancelWait hAsync = cancel hAsync >> wait hAsync 
--- tests/TestUtils.hs
+++ tests/TestUtils.hs
@@ -3,13 +3,20 @@
 
 module TestUtils
   ( TestResult
-  , TestProcessControl
-  , Ping(Ping)
-  , startTestProcess
-  , delayedAssertion
-  , assertComplete
   , noop
   , stash
+  -- ping !
+  , Ping(Ping)
+  , ping
+  -- test process utilities
+  , TestProcessControl
+  , startTestProcess
+  , runTestProcess
+  , testProcessGo
+  , testProcessStop
+  , testProcessReport
+  , delayedAssertion
+  , assertComplete
   ) where
 
 import Prelude hiding (catch)
@@ -31,33 +38,53 @@ import Control.Monad (forever)
 import Test.HUnit (Assertion)
 import Test.HUnit.Base (assertBool)
 
+-- | A mutable cell containing a test result.
 type TestResult a = MVar a
 
+-- | A simple @Ping@ signal
 data Ping = Ping
-    deriving (Typeable)
+    deriving (Typeable, Eq, Show)
 $(derive makeBinary ''Ping)
 
+ping :: ProcessId -> Process ()
+ping pid = send pid Ping
+
+-- | Control signals used to manage /test processes/
 data TestProcessControl = Stop | Go | Report ProcessId
     deriving (Typeable)
 $(derive makeBinary ''TestProcessControl)
 
+-- | Starts a test process on the local node.
 startTestProcess :: Process () -> Process ProcessId
-startTestProcess proc = spawnLocal $ testProcess proc
+startTestProcess proc = spawnLocal $ runTestProcess proc
 
-testProcess :: Process () -> Process ()
-testProcess proc = forever $ do
+-- | Runs a /test process/ around the supplied @proc@, which is executed
+-- whenever the outer process loop receives a 'Go' signal.
+runTestProcess :: Process () -> Process ()
+runTestProcess proc = forever $ do
   ctl <- expect
   case ctl of
     Stop     -> terminate
     Go       -> proc
-    Report p -> acquireAndRespond p
-  where acquireAndRespond :: ProcessId -> Process ()
-        acquireAndRespond p = do
-          _ <- receiveWait [
-              matchAny (\m -> forward m p)
-            ]
-          return ()
+    Report p -> receiveWait [matchAny (\m -> forward m p)] >> return ()
+
+-- | Tell a /test process/ to continue executing 
+testProcessGo :: ProcessId -> Process ()
+testProcessGo pid = (say $ (show pid) ++ " go!") >> send pid Go
+
+-- | Tell a /test process/ to stop (i.e., 'terminate')
+testProcessStop :: ProcessId -> Process ()
+testProcessStop pid = (say $ (show pid) ++ " stop!") >> send pid Stop
+
+-- | Tell a /test process/ to send a report (message)
+-- back to the calling process
+testProcessReport :: ProcessId -> Process ()
+testProcessReport pid = do
+  self <- getSelfPid
+  send pid $ Report self
           
+-- | Run the supplied @testProc@ using an @MVar@ to collect and assert
+-- against its result. Uses the supplied @note@ if the assertion fails.
 delayedAssertion :: (Eq a) => String -> LocalNode -> a ->
                     (TestResult a -> Process ()) -> Assertion
 delayedAssertion note localNode expected testProc = do
@@ -65,14 +92,17 @@ delayedAssertion note localNode expected testProc = do
   _ <- forkProcess localNode $ testProc result
   assertComplete note result expected
 
+-- | Takes the value of @mv@ (using @takeMVar@) and asserts that it matches @a@
 assertComplete :: (Eq a) => String -> MVar a -> a -> IO ()
 assertComplete msg mv a = do
-    b <- takeMVar mv
-    assertBool msg (a == b)
+  b <- takeMVar mv
+  assertBool msg (a == b)
 
+-- | Does exactly what it says on the tin, doing so in the @Process@ monad.
 noop :: Process ()
 noop = return ()
 
+-- | Stashes a value in our 'TestResult' using @putMVar@
 stash :: TestResult a -> a -> Process ()
 stash mvar x = liftIO $ putMVar mvar x
 
