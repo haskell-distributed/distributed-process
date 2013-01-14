960fa806de231f1c1d26e0b12871e6b1ae2df613
Tim Watson <tim@rabbitmq.com>
2013-01-14 14:44:54 +0000

    extend GenProcess to handleInfo

--- src/Control/Distributed/Process/Platform/GenProcess.hs
+++ src/Control/Distributed/Process/Platform/GenProcess.hs
@@ -15,18 +15,10 @@ module Control.Distributed.Process.Platform.GenProcess where
 
 -- TODO: define API and hide internals...
 
-import Control.Applicative
+import Control.Concurrent (threadDelay)
 import Control.Distributed.Process
 import Control.Distributed.Process.Serializable
 import Control.Distributed.Process.Platform.Time
-import qualified Control.Monad.State as ST
-  ( StateT
-  , get
-  , lift
-  , modify
-  , put
-  , runStateT
-  )
 import Data.Binary
 import Data.DeriveTH
 import Data.Typeable (Typeable)
@@ -61,10 +53,10 @@ data InitResult s =
   | forall r. (Serializable r) => InitStop r
 
 data ProcessAction s =
-    ProcessContinue { nextState :: s }
-  | ProcessTimeout  { delay :: Delay, nextState :: s }
-  | ProcessHibernate { delay :: Delay, nextState :: s }
-  | ProcessStop     { reason :: TerminateReason } 
+    ProcessContinue  { nextState :: s }
+  | ProcessTimeout   { delay :: TimeInterval, nextState :: s }
+  | ProcessHibernate { duration :: TimeInterval, nextState :: s }
+  | ProcessStop      { reason :: TerminateReason } 
 
 data ProcessReply s a =
     ProcessReply { response :: a
@@ -74,15 +66,10 @@ data ProcessReply s a =
 type InitHandler      a s   = a -> InitResult s
 type TerminateHandler s     = s -> TerminateReason -> Process ()
 type TimeoutHandler   s     = s -> Delay -> Process (ProcessAction s)
-type CallHandler      s a b = s -> a -> Process (ProcessReply s b)
-type CastHandler      s a   = s -> a -> Process (ProcessAction s)
-
-data Req a b = Req a b 
 
 -- dispatching to implementation callbacks
 
--- | Dispatcher that knows how to dispatch messages to a handler
--- s The server state
+-- | this type defines dispatch from abstract messages to a typed handler
 data Dispatcher s =
     forall a . (Serializable a) => Dispatch {
         dispatch :: s -> Message a -> Process (ProcessAction s)
@@ -91,37 +78,38 @@ data Dispatcher s =
         dispatch   :: s -> Message a -> Process (ProcessAction s)
       , dispatchIf :: s -> Message a -> Bool
       }
-  | forall a . (Serializable a) => DispatchReply {
-        handle   :: s -> Message a -> Process (ProcessAction s)
-      }
-  | forall a . (Serializable a) => DispatchReplyIf {
-        handle   :: s -> Message a -> Process (ProcessAction s)
-      , handleIf :: s -> Message a -> Bool
-      }
+  | DispatchInfo {
+      dispatchInfo :: UnhandledMessagePolicy
+                   -> s
+                   -> AbstractMessage
+                   -> Process (ProcessAction s)
+    }
 
--- | Matches messages using a dispatcher
+-- | matches messages of specific types using a dispatcher
 class MessageMatcher d where
-    matchMessage :: s -> d s -> Match (ProcessAction s)
+    matchMessage :: UnhandledMessagePolicy -> s -> d s -> Match (ProcessAction s)
 
--- | Matches messages to a MessageDispatcher
+-- | matches messages to a MessageDispatcher
 instance MessageMatcher Dispatcher where
-  matchMessage s (Dispatch        d)      = match (d s)
-  matchMessage s (DispatchIf      d cond) = matchIf (cond s) (d s)
-  matchMessage s (DispatchReply   d)      = match (d s)
-  matchMessage s (DispatchReplyIf d cond) = matchIf (cond s) (d s)
+  matchMessage _ s (Dispatch        d)      = match (d s)
+  matchMessage _ s (DispatchIf      d cond) = matchIf (cond s) (d s)
+  matchMessage p s (DispatchInfo    d)      = matchAny (d p s)
+
+-- | Policy for handling unexpected messages, i.e., messages which are not
+-- sent using the 'call' or 'cast' APIs, and which are not handled by any of the
+-- 'handleInfo' handlers.
+data UnhandledMessagePolicy =
+    Terminate
+  | DeadLetter ProcessId
+  | Drop 
 
 data Behaviour s = Behaviour {
     dispatchers      :: [Dispatcher s]
+  , infoHandlers     :: [Dispatcher s]
   , timeoutHandler   :: TimeoutHandler s
   , terminateHandler :: TerminateHandler s   -- ^ termination handler
+  , unhandledMessagePolicy :: UnhandledMessagePolicy
   }
-
--- sending replies
-
-replyTo :: (Serializable m) => Recipient -> m -> Process ()
-replyTo (SendToPid p) m = send p m
-replyTo (SendToService s) m = nsend s m
-replyTo (SendToRemoteService s n) m = nsendRemote n s m
 
 --------------------------------------------------------------------------------
 -- Cloud Haskell Generic Process API                                          --
@@ -130,24 +118,45 @@ replyTo (SendToRemoteService s n) m = nsendRemote n s m
 start :: Process ()
 start = undefined
 
-stop :: Process ()
-stop = undefined 
-
 call :: Process ()
 call = undefined
 
 cast :: Process ()
 cast = undefined
 
---------------------------------------------------------------------------------
--- Constructing Handlers from *ordinary* functions                            --
---------------------------------------------------------------------------------
+-- Constructing Handlers from *ordinary* functions
 
+-- | INstructs the process to send a reply and continue working. 
+-- > reply reply' state = replyWith reply' (continue state)
 reply :: (Serializable r) => r -> s -> ProcessReply s r
-reply r s = replyWith r (ProcessContinue s)
+reply r s = replyWith r (continue s)
 
+-- | Instructs the process to send a reply and evaluate the 'ProcessAction'
+-- thereafter. 
 replyWith :: (Serializable m) => m -> ProcessAction s -> ProcessReply s m
 replyWith msg state = ProcessReply msg state 
+
+-- | Instructs the process to continue running and receiving messages.
+continue :: s -> ProcessAction s
+continue s = ProcessContinue s
+
+-- | Instructs the process to wait for incoming messages until 'TimeInterval'
+-- is exceeded. If no messages are handled during this period, the /timeout/
+-- handler will be called. Note that this alters the process timeout permanently
+-- such that the given @TimeInterval@ will remain in use until changed.  
+timeoutAfter :: TimeInterval -> s -> ProcessAction s
+timeoutAfter d s = ProcessTimeout d s
+
+-- | Instructs the process to /hibernate/ for the given 'TimeInterval'. Note
+-- that no messages will be removed from the mailbox until after hibernation has
+-- ceased. This is equivalent to calling @threadDelay@.
+-- 
+hibernate :: TimeInterval -> s -> ProcessAction s
+hibernate d s = ProcessHibernate d s
+
+-- | Instructs the process to cease, giving the supplied reason for termination.
+stop :: TerminateReason -> ProcessAction s
+stop r = ProcessStop r
 
 -- | Constructs a 'call' handler from an ordinary function in the 'Process'
 -- monad. Given a function @f :: (s -> a -> Process (ProcessReply s b))@,
@@ -155,14 +164,17 @@ replyWith msg state = ProcessReply msg state
 -- in a 'Behaviour' specification for the /GenProcess/.
 --
 handleCall :: (Serializable a, Serializable b)
-           => (s -> a -> Process (ProcessReply s b)) -> Dispatcher s
-handleCall handler = DispatchReplyIf {
-      handle = doHandle handler
-    , handleIf = doCheck 
+           => (s -> a -> Process (ProcessReply s b))
+           -> Dispatcher s
+handleCall handler = DispatchIf {
+      dispatch = doHandle handler
+    , dispatchIf = doCheck 
     }
   where doHandle :: (Serializable a, Serializable b)
-                 => (s -> a -> Process (ProcessReply s b)) -> s
-                 -> Message a -> Process (ProcessAction s)
+                 => (s -> a -> Process (ProcessReply s b))
+                 -> s
+                 -> Message a
+                 -> Process (ProcessAction s)
         doHandle h s (CallMessage p c) = (h s p) >>= mkReply c
         doHandle _ _ _ = error "illegal input"  
         -- TODO: standard 'this cannot happen' error message
@@ -170,6 +182,8 @@ handleCall handler = DispatchReplyIf {
         doCheck _ (CallMessage _ _) = True
         doCheck _ _                 = False        
         
+        -- handling 'reply-to' in the main process loop is awkward at best,
+        -- so we handle it here instead and return the 'action' to the loop
         mkReply :: (Serializable b)
                 => Recipient -> ProcessReply s b -> Process (ProcessAction s)
         mkReply _ (NoReply a) = return a
@@ -181,42 +195,114 @@ handleCall handler = DispatchReplyIf {
 -- in a 'Behaviour' specification for the /GenProcess/.
 --
 handleCast :: (Serializable a)
-           => CastHandler s a -> Dispatcher s
-handleCast h = Dispatch {
-    dispatch = (\s (CastMessage p) -> h s p)
-  }            
+           => (s -> a -> Process (ProcessAction s)) -> Dispatcher s
+handleCast h = Dispatch { dispatch = (\s (CastMessage p) -> h s p) }            
 
-loop :: Behaviour s -> s -> Delay -> Process TerminateReason
-loop b s t = do
-    ac <- processReceive s b t
-    nextAction b ac
-    where nextAction :: Behaviour s -> ProcessAction s -> Process TerminateReason
-          nextAction b (ProcessContinue s')   = loop b s' t
-          nextAction b (ProcessTimeout t' s') = loop b s' t'
-          nextAction _ (ProcessStop r)        = return (r :: TerminateReason)
+handleInfo :: forall s a. (Serializable a)
+           => (s -> a -> Process (ProcessAction s)) -> Dispatcher s
+handleInfo h = DispatchInfo {
+    dispatchInfo = dispatchIt h
+  }
+  where dispatchIt :: (Serializable a) 
+                   => (s -> a -> Process (ProcessAction s))
+                   -> UnhandledMessagePolicy
+                   -> s
+                   -> AbstractMessage -> Process (ProcessAction s)
+        dispatchIt h' pol s msg = do
+            m <- maybeHandleMessage msg (h' s)
+            case m of
+                Nothing -> applyPolicy s pol msg 
+                Just act -> return (act :: ProcessAction s) 
 
-processReceive :: s -> Behaviour s -> Delay -> Process (ProcessAction s)
-processReceive s b t =
-    let ms = map (matchMessage s) (dispatchers b) in do
+-- Process Implementation
+
+applyPolicy :: s
+            -> UnhandledMessagePolicy
+            -> AbstractMessage
+            -> Process (ProcessAction s)
+applyPolicy s p m = do
+  case p of
+    Terminate -> return $ stop (TerminateOther "unexpected-input")
+    DeadLetter pid -> forward m pid >> return (continue s)
+    Drop -> return (continue s)      
+
+initLoop :: Behaviour s -> s -> Process TerminateReason
+initLoop b s =
+  let p   = unhandledMessagePolicy b
+      t   = timeoutHandler b 
+      ms  = map (matchMessage p s) (dispatchers b)
+      ms' = addInfoHandlers b s p ms
+  in loop ms' t s Infinity
+  where
+    addInfoHandlers :: Behaviour s
+                    -> s
+                    -> UnhandledMessagePolicy
+                    -> [Match (ProcessAction s)]
+                    -> [Match (ProcessAction s)] 
+    addInfoHandlers b' s' p rms =
+        rms ++ addInfoAux p s' (infoHandlers b') []
+    
+    -- if there's more than one info handler then we /do not/ want to apply the
+    -- policy until we reach the last one, otherwise we'll miss out the others
+    addInfoAux :: UnhandledMessagePolicy
+               -> s
+               -> [Dispatcher s]
+               -> [Match (ProcessAction s)]
+               -> [Match (ProcessAction s)]
+    addInfoAux _ _ [] _ = []
+    addInfoAux p s'' (d:ds :: [Dispatcher s]) acc
+        | length ds == 0  = reverse ((matchMessage p s'' d):acc)
+        | otherwise = ((matchMessage Drop s'' d):(addInfoAux p s'' ds acc))
+    
+loop :: [Match (ProcessAction s)]
+     -> TimeoutHandler s
+     -> s
+     -> Delay
+     -> Process TerminateReason
+loop ms h s t = do
+    ac <- processReceive ms h s t
+    case ac of
+      (ProcessContinue s') -> loop ms h s' t
+      (ProcessTimeout t' s') -> loop ms h s' (Delay t')
+      (ProcessHibernate d' s') -> block d' >> loop ms h s' t
+      (ProcessStop r) -> return (r :: TerminateReason)
+  where block :: TimeInterval -> Process ()
+        block i = liftIO $ threadDelay (asTimeout i)
+
+processReceive :: [Match (ProcessAction s)]
+               -> TimeoutHandler s
+               -> s
+               -> Delay
+               -> Process (ProcessAction s)
+processReceive ms h s t = do
     next <- recv ms t
     case next of
-        Nothing -> (timeoutHandler b) s t
+        Nothing -> h s t
         Just pa -> return pa
   where
-    recv :: [Match (ProcessAction s)] -> Delay -> Process (Maybe (ProcessAction s))
+    recv :: [Match (ProcessAction s)]
+         -> Delay
+         -> Process (Maybe (ProcessAction s))
     recv matches d =
         case d of
             Infinity -> receiveWait matches >>= return . Just
-            Delay t' -> receiveTimeout (asTimeout t') matches
+            Delay t' -> receiveTimeout (asTimeout t') matches  
+
+-- internal/utility
+
+replyTo :: (Serializable m) => Recipient -> m -> Process ()
+replyTo (SendToPid p) m             = send p m
+replyTo (SendToService s) m         = nsend s m
+replyTo (SendToRemoteService s n) m = nsendRemote n s m
 
 demo :: Behaviour [String]
-demo =
-  Behaviour {
-      dispatchers = [
-          handleCall add
-      ]
-    , terminateHandler = undefined
-    }
+demo = Behaviour {
+     dispatchers = [
+         handleCall add
+       ]
+   , infoHandlers = []
+   , terminateHandler = undefined
+   }
 
 add :: [String] -> String -> Process (ProcessReply [String] String)
 add s x =
