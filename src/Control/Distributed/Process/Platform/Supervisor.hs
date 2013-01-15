9aa21e049e37d297feccd7bb87d9fccb59a20a7f
Tim Watson <tim@rabbitmq.com>
2013-01-15 01:31:07 +0000

    more API niceness

--- src/Control/Distributed/Process/Platform/Supervisor.hs
+++ src/Control/Distributed/Process/Platform/Supervisor.hs
@@ -4,11 +4,22 @@
 {-# LANGUAGE TemplateHaskell            #-}
 
 module Control.Distributed.Process.Platform.Supervisor 
-  ( foo
+  ( SupervisorStats(..)
+  , ChildId
+  , Child(..)
+  , ChildKey
+  , ChildType
+  , ChildSpec
+  , toPid
+  , start
+  , statistics
+  , addChild
+  , lookupChild
   ) where
 
-import Control.Distributed.Process
-import Control.Distributed.Process.Platform.GenProcess
+import Control.Distributed.Process hiding (call)
+import Control.Distributed.Process.Platform.GenProcess hiding (start)
+import qualified Control.Distributed.Process.Platform.GenProcess as Server
 import Control.Distributed.Process.Platform.Internal.Types
 import Control.Distributed.Process.Platform.Time
 
@@ -26,16 +37,19 @@ import Prelude hiding (init)
      check_childspecs/1]).
 -}
 
+--------------------------------------------------------------------------------
+-- Cloud Haskell Generic Supervisor API                                       --
+--------------------------------------------------------------------------------
+
 -- TODO: we need process id *and* monitor refs, plus all the startup info
+
+-- type SupervisorRef = ServerId
 
 type ChildId = Int
 
 newtype Child = Child ProcessId
     deriving (Eq, Show, Typeable)
 $(derive makeBinary ''Child)
-
-defaultChild :: Child
-defaultChild = Child (undefined :: ProcessId)
 
 data ChildKey = ChildKey !ChildId !Child
     deriving (Typeable, Show)
@@ -47,66 +61,195 @@ instance Eq ChildKey where
 instance Ord ChildKey where
   (ChildKey x _) `compare` (ChildKey y _) = x `compare` y
 
-data ChildSpec = ChildSpec
-    deriving (Typeable, Show)
+data ChildType = Worker | Supervisor
+    deriving (Typeable, Show, Eq)
+$(derive makeBinary ''ChildType)
+
+data ChildSpec = ChildSpec {
+    childId   :: ChildKey
+  , childType :: ChildType
+  } deriving (Typeable, Show, Eq)
 $(derive makeBinary ''ChildSpec)
+
+data SupervisorStats = SupervisorStats {
+    childSpecCount :: Int
+  , childSupervisorCount :: Int
+  , childWorkerCount :: Int
+  , activeChildCount :: Int
+  , activeSupervisorCount :: Int
+  , activeWorkerCount :: Int
+  -- TODO: usage/restart/freq stats
+  , totalRestarts :: Int
+  } deriving (Typeable, Show)
+$(derive makeBinary ''SupervisorStats)
+
+-- service API types
+
+data ApiCallFind = ApiCallFind ChildKey
+    deriving (Typeable, Show, Eq)
+$(derive makeBinary ''ApiCallFind)
+
+data ApiCallAdd = ApiCallAdd ChildSpec
+    deriving (Typeable, Show, Eq)
+$(derive makeBinary ''ApiCallAdd)
+
+data ApiCallStats = ApiCallStats
+    deriving (Typeable, Show, Eq)
+$(derive makeBinary ''ApiCallStats)
+
+-- supervisor state
 
 -- | Supervisor's state - most of our lookups are id based
 data State = State {
-    specs :: Map.Map ChildKey ChildSpec
-  , maxId :: Int
+    specs  :: Map.Map ChildKey ChildSpec
+  , active :: Map.Map ProcessId ChildId
+  , stats  :: SupervisorStats
+  , maxId  :: Int   -- TODO: use Map.findMax instead...
   }
+
+-- start/specify
+
+start :: Process ProcessId
+start =
+  let srv = supServer
+  in spawnLocal $ Server.start () supInit srv >> return ()
+
+-- client API
 
 toPid :: ChildKey -> ProcessId
 toPid (ChildKey _ (Child p)) = p
 
-foo :: Process ()
-foo = undefined
+statistics :: ProcessId -> Process (Maybe SupervisorStats)
+statistics = (flip safeCall) ApiCallStats
+
+lookupChild :: ProcessId -> ChildKey -> Process (Maybe ChildSpec)
+lookupChild sid = safeCall sid . Just . ApiCallFind
+
+addChild :: ProcessId -> ChildSpec -> Process (Maybe ChildKey)
+addChild sid = safeCall sid . Just . ApiCallAdd
 
 -- callback API
 
 handleLookupChild :: State
-                  -> ChildKey
+                  -> ApiCallFind
                   -> Process (ProcessReply State (Maybe ChildSpec))
-handleLookupChild state key = reply (spec state key) state
+handleLookupChild state (ApiCallFind key) = reply (spec state key) state
   where spec :: State -> ChildKey -> Maybe ChildSpec 
         spec s k = Map.lookup k (specs s)
 
 handleAddChild :: State
-               -> ChildSpec
+               -> ApiCallAdd
                -> Process (ProcessReply State (Maybe ChildKey))
-handleAddChild state spec =
-  let childId = nextKey state
-  in do
-    child <- tryStartChild childId spec
-    case child of
-      err@(Left _) -> reply Nothing state
-      Right key -> store key spec state{ maxId = childId } >>= reply (Just key)              
+handleAddChild state (ApiCallAdd spec) =
+  let childId' = nextId state
+      key      = ChildKey childId' emptyChildPid
+      spec'    = spec{ childId = key }
+  in store key spec' state >>= reply (Just key)
+  
+--  child <- tryStartChild state spec
+--  case child of
+--    err@(Left _) -> reply Nothing state
+--    Right key -> markActive key spec state >>= reply (Just key)              
 
-tryStartChild :: ChildId
+handleGetStats :: State -> ApiCallStats -> Process (ProcessReply State SupervisorStats)
+handleGetStats s ApiCallStats = reply (stats s) s
+
+-- info APIs
+
+handleMonitorSignal :: State
+                    -> ProcessMonitorNotification
+                    -> Process (ProcessAction State)
+handleMonitorSignal state (ProcessMonitorNotification _ pid _reason) =
+  let m = active state
+      (cId, active') = Map.updateLookupWithKey (\_ _ -> Nothing) pid m 
+  in handleChildDown cId active' state  
+
+-- internal/aux APIs
+
+handleChildDown :: Maybe ChildId
+                -> Map.Map ProcessId ChildId
+                -> State
+                -> Process (ProcessAction State)
+handleChildDown cId active' state =
+  let cSpec = case cId of
+                Nothing -> undefined :: Maybe ChildSpec
+                Just c  -> Map.lookup (initKey c) (specs state)
+  in do
+    -- restart it
+    -- change the state
+    -- bump stats
+    say $ "hmn, what to do with " ++ (show cSpec)
+    continue $ state{ active = active' }
+
+initKey :: ChildId -> ChildKey
+initKey = (flip ChildKey) emptyChildPid
+
+emptyChildPid :: Child
+emptyChildPid = Child (undefined :: ProcessId)
+
+tryStartChild :: State
               -> ChildSpec
               -> Process (Either TerminateReason ChildKey)
-tryStartChild childId spec = undefined
+tryStartChild _ _ = undefined
 
 store :: ChildKey -> ChildSpec -> State -> Process State
-store key spec state =
-  let specs' = Map.insert key spec (specs state) 
-  in return state{ specs = specs' }
+store key@(ChildKey chId _) spec state =
+  let chType = childType spec
+      specs' = Map.insert key spec (specs state)
+      stats' = bumpStats chType $ stats state
+  in return state{ specs = specs'
+                 , maxId = chId
+                 , stats = stats' }
 
-nextKey :: State -> Int
-nextKey = (+1) . maxId
+bumpStats :: ChildType -> SupervisorStats -> SupervisorStats
+bumpStats Worker s =
+  let cs = childSpecCount s
+      cw = childWorkerCount s
+  in s{ childSpecCount = cs, childWorkerCount = cw }
+bumpStats Supervisor s =
+  let cs = childSpecCount s
+      sw = childSupervisorCount s
+  in s{ childSpecCount = cs, childSupervisorCount = sw }
+
+nextId :: State -> Int
+nextId = (+1) . maxId
 
 -- GenProcess API
 
-supInit :: InitHandler [String] State
-supInit _ = return $ InitOk State { specs = Map.empty, maxId = 0 } Infinity
+-- TODO: allow /init/ with existing spec list and start 'em up immediately
+
+supInit :: InitHandler () State
+supInit _ = return $ InitOk emptyState Infinity
+
+emptyState :: State
+emptyState = State {
+    specs  = Map.empty
+  , active = Map.empty
+  , maxId  = 0
+  , stats  = emptyStats
+  }
+
+emptyStats :: SupervisorStats
+emptyStats = SupervisorStats {
+    childSpecCount        = 0
+  , childSupervisorCount  = 0
+  , childWorkerCount      = 0
+  , activeChildCount      = 0
+  , activeSupervisorCount = 0
+  , activeWorkerCount     = 0
+  , totalRestarts         = 0
+--  , avgRestartFrequency   = 0
+  }
+  -- TODO: usage/restart/freq stats
 
 supServer :: Behaviour State
 supServer = Behaviour {
      dispatchers = [
          handleCall handleLookupChild
+       , handleCall handleAddChild
+       , handleCall handleGetStats
        ]
-   , infoHandlers = []
+   , infoHandlers = [handleInfo handleMonitorSignal]
    , timeoutHandler = undefined
    , terminateHandler = undefined
    , unhandledMessagePolicy = Drop 
