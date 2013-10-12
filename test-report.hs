ecd5d3232644657b21b1716610a045410c0c01ba
Tim Watson <watson.timothy@gmail.com>
2013-10-12 19:44:04 +0100

    Use cabal-dev for hpc report generation

--- test-report.hs
+++ test-report.hs
@@ -2,9 +2,9 @@
 
 HPC_DIR=dist/hpc
 
-cabal clean
-cabal configure --enable-tests --enable-library-coverage
-cabal build
-cabal test
+cabal-dev clean
+cabal-dev configure --enable-tests --enable-library-coverage
+cabal-dev build
+cabal-dev test
 
 open ${HPC_DIR}/html/*/hpc-index.html
