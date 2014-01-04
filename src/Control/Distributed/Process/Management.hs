fe0a762983400e1829c0cd500110b4d5e6f44356
Tim Watson <watson.timothy@gmail.com>
2014-01-04 13:56:19 +0000

    Improve Management API Documentation

--- src/Control/Distributed/Process/Management.hs
+++ src/Control/Distributed/Process/Management.hs
@@ -51,19 +51,58 @@
 -- to the agent's event sinks from both sources, so an agent need only concern
 -- itself with how to respond to its inputs.
 --
+-- Some agents may wish to prioritise messages from their mailbox over traffic
+-- on the management event bus, or vice versa. The 'mxReceive' and
+-- 'mxReceiveChan' API calls do this for the mailbox and event bus,
+-- respectively. The /prioritisation/ these APIs offer is simply that the chosen
+-- data stream will be checked first. No blocking will occur if the chosen
+-- (prioritised) source is devoid of input messages, instead the agent handling
+-- code will revert to switching between the alternatives in /round-robin/ as
+-- usual. This switching (and prioritisation) will take place a limited number
+-- of times, after which - in order to avoid over-utilising system
+-- resources - the agent will revert to blocking on the event bus (and ignoring
+-- its mailbox).
+--
+-- Prioritisation then, is a /hint/ about the preference of data source from
+-- which the next input should be chosen. No guarantee can be made that the
+-- chosen source will in fact be selected at runtime.
+--
+-- [Management API Semantics]
+--
+-- The management API provides /no guarantees whatsoever/, viz:
+--
+--  * The ordering of messages delivered to the event bus.
+--
+--  * The order in which agents will be executed.
+--
+--  * Whether messages will be taken from the mailbox first, or the event bus.
+--
 -- [Management Data API]
 --
 -- Both management agents and clients of the API have access to a variety of
--- data storage capabilities, to facilitate publishing and consuming information
--- obtained by agents. Each agent is assigned its own data table, which acts
--- as a shared map, where the keys are @String@s and the values are
--- @Serializable@ datum of whatever type the agent or its clients stores.
+-- data storage capabilities, to facilitate publishing and consuming useful
+-- system information. Agents maintain their own internal state privately (via a
+-- state transformer - see 'mxGetLocal' et al), however it is possible for
+-- agents to share additional data with each other (and the outside world)
+-- using /data tables/.
+--
+-- Each agent is assigned its own data table, which acts as a shared map, where
+-- the keys are @String@s and the values are @Serializable@ datum of whatever
+-- type the agent or its clients stores.
+--
+-- Because an agent's /data table/ stores its values in raw 'Message' format,
+-- it works effectively as an /un-typed dictionary/, into which data of varying
+-- types can be fed and later retrieved. The upside of this is that different
+-- keys can be mapped to various types without any additional work on the part
+-- of the developer. The downside is that the code reading these values must
+-- know in advance what type(s) to expect, and the API provides no additional
+-- support for handling that.
 --
 -- Publishing is accomplished using the 'mxPublish' and 'mxSet' APIs, whilst
 -- querying and deletion are handled by 'mxGet', 'mxClear', 'mxPurgeTable' and
 -- 'mxDropTable' respectively.
 --
--- When management agents terminate, their tables are left in memory despite the
+-- When a management agent terminates, their tables are left in memory despite
 -- termination, such that an agent may resume its role (by restarting) or have
 -- its 'MxAgentId' taken over by another subsequent agent, leaving the data
 -- originally captured in place.
@@ -89,18 +128,78 @@
 -- will obtain data from the event bus and process' mailbox in a round robbin
 -- fashion, i.e., one after the other, changing each time.
 --
+-- [Example Code]
+--
+-- What follows is a grossly over-simplified example of a management agent that
+-- provides a basic name monitoring facility. Whenever a process name is
+-- registered or unregistered, clients are informed of the fact.
+--
+-- > -- simple notification data type
+-- >
+-- > data Registration = Reg { added  :: Bool
+-- >                         , procId :: ProcessId
+-- >                         , name   :: String
+-- >                         }
+-- >
+-- > -- start a /name monitoring agent/
+-- > nameMonitorAgent = do
+-- >   mxAgent (MxAgentId "name-monitor") Set.empty [
+-- >         (mxSink $ \(pid :: ProcessId) -> do
+-- >            mxUpdateState $ Set.insert pid
+-- >            mxReady)
+-- >       , (mxSink $
+-- >             let act =
+-- >                   case ev of
+-- >                     (MxRegistered   p n) -> notify True  n p
+-- >                     (MxUnRegistered p n) -> notify False n p
+-- >                     _                    -> return ()
+-- >             act >> mxReady)
+-- >     ]
+-- >   where
+-- >     notify a n p = do
+-- >       Foldable.mapM_ (liftMX . deliver (Reg a n p)) =<< mxGetLocal
+-- >
+--
+-- The client interface (for sending their pid) can take one of two forms:
+--
+-- > monitorNames = getSelfPid >>= nsend "name-monitor"
+-- > monitorNames2 = getSelfPid >>= mxNotify
+--
+-- For some real-world examples, see the distributed-process-platform package.
+--
+-- [Performance, Stablity and Scalability]
+--
+-- /Management Agents/ offer numerous advantages over regular processes:
+-- broadcast communication with them can have a lower latency, they offer
+-- simplified messgage (i.e., input type) handling and they have access to
+-- internal system information that would be otherwise unobtainable.
+--
+-- Do not be tempted to implement everything (e.g., the kitchen sink) using the
+-- management API though. There are overheads associated with management agents
+-- which is why they're presented as tools for consuming low level system
+-- information, instead of as /application level/ development tools.
+--
+-- Agents that rely heavily on a busy mailbox can cause the management event
+-- bus to backlog un-GC'ed data, leading to increased heap space. Producers that
+-- do not take care to avoid passing unevaluated thunks to the API can crash
+-- /all/ the agents in the system. Agents are not monitored or managed in any
+-- way, and those that crash will not be restarted.
+--
+-- The management event bus can receive a great deal of traffic. Every time
+-- a message is sent and/or received, an event is passed to the agent controller
+-- and broadcast to all agents (plus the trace controller, if tracing is enabled
+-- for the node). This is already a significant overhead - though profiling and
+-- benchmarks have demonstrated that it does not adversely affect performance
+-- if few agents are installed. Agents will typically use more cycles than plain
+-- processes, since they perform additional work: selecting input data from both
+-- the event bus /and/ their own mailboxes, plus searching through the set of
+-- event sinks (for each agent) to determine the right handler for the event.
+--
+-- Each management agent requires not only its own @Process@ (in which the agent
+-- code is run), but also a peer process that provides its /data table/. These
+-- data tables also have to be coordinated and manaaged on each agent's behalf.
+--
 -- [Architecture Overview]
---
--- /Management Extensions/ can be used by infrastructure or application code,
--- to provide access to runtime instrumentation data, expose control planes
--- and/or provide remote access to external clients.
---
--- The management infrastructure is broken down into three components, which
--- roughly correspond to the modules in which they're implemented:
---
--- 1. @Agent@  - registration and definition of /Management Agents/
--- 2. @Table@  - provides an API for publishing /instrumentation data/
--- 3. @Trace@  - provides an API for tracing/debugging capabilities
 --
 -- The architecture of the management event bus is internal and subject to
 -- change without prior notice. The description that follows is provided for
@@ -131,7 +230,7 @@
 -- The management control plane is driven by a shared broadcast channel, which
 -- is written to by the agent controller and subscribed to by all agent
 -- processes. Agents are spawned as regular processes, whose primary
--- implementation (i.e., their "server loop") is responsible for consuming
+-- implementation (i.e., /server loop/) is responsible for consuming
 -- messages from both the broadcast channel and their own mailbox. Once
 -- consumed, messages are applied to the agent's /event sinks/ until one
 -- matches the input, at which point it is applied and the loop continues.
@@ -241,9 +340,8 @@ mxNotify msg = do
 
 -- | Publish an arbitrary @Message@ as a property in the management database.
 --
--- For publishing @Serializable@ data, use 'mxSet' instead. For publishing
--- properties efficiently within an agent, use 'mxAgentPublish' instead,
--- since it will be more efficient in that context.
+-- For publishing @Serializable@ data, use 'mxSet' instead.
+--
 mxPublish :: MxAgentId -> String -> Message -> Process ()
 mxPublish a k v = Table.set k v (Table.MxForAgent a)
 
