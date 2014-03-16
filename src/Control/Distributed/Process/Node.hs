e944e7d27808c69ac0c087b8eea552d8b45d4213
Tavis Rudd <tavis@damnsimple.com>
2014-03-16 14:37:16 -0700

    revert export of LocalNode/LocalNodeState constuctors in favour of accessors
    See
    https://github.com/haskell-distributed/distributed-process/pull/146#issuecomment-37753620

--- src/Control/Distributed/Process/Node.hs
+++ src/Control/Distributed/Process/Node.hs
@@ -8,14 +8,15 @@
 -- | Local nodes
 --
 module Control.Distributed.Process.Node
-  ( LocalNode(..)
-  , LocalNodeState(..)
+  ( LocalNode
+  , LocalNodeState -- opaque, exported for debug introspection via accessors
   , newLocalNode
   , closeLocalNode
   , forkProcess
   , runProcess
   , initRemoteTable
   , localNodeId
+  , localState -- StrictMVar LocalNodeState on LocalNode, exported for debugging
   ) where
 
 -- TODO: Calls to 'sendBinary' and co (by the NC) may stall the node controller.
