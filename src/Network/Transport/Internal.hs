e0eef88b41835ff2a528a35fe0c3ae7f4cdf29eb
Facundo Domínguez <facundo.dominguez@tweag.io>
2017-02-23 13:11:49 -0300

    Import <$> explicitly for old ghcs.

--- src/Network/Transport/Internal.hs
+++ src/Network/Transport/Internal.hs
@@ -37,6 +37,7 @@ import qualified Data.ByteString.Internal as BSI
   , inlinePerformIO
   )
 import Data.Word (Word32, Word16)
+import Control.Applicative ((<$>))
 import Control.Monad.IO.Class (MonadIO, liftIO)
 import Control.Exception
   ( IOException
