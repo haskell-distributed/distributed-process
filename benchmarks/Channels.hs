391cb937d1d0c96b1fb586b917b2e1df107b659d
Tim Watson <watson.timothy@gmail.com>
2018-11-11 21:02:33 +0000

    adjust to deal with the latest network-transport-tcp API changes and refactor benchmarks and test runner accordingly

--- benchmarks/Channels.hs
+++ benchmarks/Channels.hs
@@ -4,7 +4,7 @@ import Control.Monad
 import Control.Applicative
 import Control.Distributed.Process
 import Control.Distributed.Process.Node
-import Network.Transport.TCP (createTransport, defaultTCPParameters)
+import Network.Transport.TCP (createTransport, defaultTCPAddr, defaultTCPParameters)
 import Data.Binary (encode, decode)
 import qualified Data.ByteString.Lazy as BSL
 
@@ -36,7 +36,8 @@ initialProcess "CLIENT" = do
 main :: IO ()
 main = do
   [role, host, port] <- getArgs
-  Right transport <- createTransport
-      host port (\sn -> (host, port)) defaultTCPParameters
-  node <- newLocalNode transport initRemoteTable
-  runProcess node $ initialProcess role
+  trans <- createTransport (defaultTCPAddr host port) defaultTCPParameters
+  case trans of
+    Right transport -> do node <- newLocalNode transport initRemoteTable
+                          runProcess node $ initialProcess role
+    Left other -> error $ show other
