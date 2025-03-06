b4b07a1f4a2dc8791ef0b5adf623216265730935
Tim McGilchrist <timmcgil@gmail.com>
2025-03-06 15:25:53 +1100

    Move ChatServer/Client example into network-transport-tcp.

--- packages/network-transport-tcp/tests/chat/ChatServer.hs
+++ packages/network-transport-tcp/tests/chat/ChatServer.hs
@@ -1,6 +1,8 @@
+module Main (main) where
+
 import System.Environment (getArgs)
 import Network.Transport
-import Network.Transport.TCP (createTransport)
+import Network.Transport.TCP (createTransport, defaultTCPAddr, defaultTCPParameters)
 import Control.Monad.State (evalStateT, modify, get)
 import Control.Monad (forever)
 import Control.Monad.IO.Class (liftIO)
@@ -10,7 +12,7 @@ import qualified Data.ByteString.Char8 as BSC (pack)
 main :: IO ()
 main = do
   host:port:_     <- getArgs
-  Right transport <- createTransport host port
+  Right transport <- createTransport (defaultTCPAddr host port) defaultTCPParameters
   Right endpoint  <- newEndPoint transport
 
   putStrLn $ "Chat server ready at " ++ (show . endPointAddressToByteString . address $ endpoint)
@@ -20,9 +22,10 @@ main = do
     case event of
       ConnectionOpened cid _ addr -> do
         get >>= \clients -> liftIO $ do
-          Right conn <- connect endpoint addr ReliableOrdered
-          send conn [BSC.pack . show . IntMap.elems $ clients]
+          Right conn <- connect endpoint addr ReliableOrdered defaultConnectHints
+          _ <- send conn [BSC.pack . show . IntMap.elems $ clients]
           close conn
-        modify $ IntMap.insert cid (endPointAddressToByteString addr)
+        modify $ IntMap.insert (fromIntegral cid) (endPointAddressToByteString addr)
       ConnectionClosed cid ->
-        modify $ IntMap.delete cid
+        modify $ IntMap.delete (fromIntegral cid)
+      _ -> liftIO . putStrLn $ "Other event received"
